2c20bd108585d3cad620c11f5b6a8d03274353b4
Clement Escoffier <clement@apache.org>
2007-12-21 19:28:07 +0000

    Commit the iPOJO Split. The composites are now in a different bundle, and it is possible to extend iPOJO by implementing new factories managing other content type (as script, bpel ...).
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/clement/ipojo/composite@606280 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/felix/ipojo/composite/CompositeFactory.java
@@ -0,0 +1,173 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.composite;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+
+import org.apache.felix.ipojo.ComponentFactory;
+import org.apache.felix.ipojo.ComponentInstance;
+import org.apache.felix.ipojo.ConfigurationException;
+import org.apache.felix.ipojo.Factory;
+import org.apache.felix.ipojo.Handler;
+import org.apache.felix.ipojo.HandlerManager;
+import org.apache.felix.ipojo.IPojoContext;
+import org.apache.felix.ipojo.MissingHandlerException;
+import org.apache.felix.ipojo.UnacceptableConfiguration;
+import org.apache.felix.ipojo.metadata.Element;
+import org.apache.felix.ipojo.util.Logger;
+import org.apache.felix.ipojo.util.Tracker;
+import org.apache.felix.ipojo.util.TrackerCustomizer;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.InvalidSyntaxException;
+
+/**
+ * The component factory manages component instance objects. This management
+ * consist in creating and managing component instance build with the component
+ * factory. This class could export Factory and ManagedServiceFactory services.
+ * 
+ * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+ */
+public class CompositeFactory extends ComponentFactory implements TrackerCustomizer {
+
+    /**
+     * Tracker used to track required handler factories.
+     */
+    protected Tracker m_tracker;
+
+    /**
+     * Create a composite factory.
+     * @param bc : bundle context
+     * @param cm : metadata of the component to create
+     */
+    public CompositeFactory(BundleContext bc, Element cm) {
+        super(bc, cm);
+        if (! check(cm)) {
+            return;
+        }      
+    }
+    
+    /**
+     * Check if the metadata are well formed.
+     * @param cm : metadata
+     * @return true if the metadata are correct.
+     * @see org.apache.felix.ipojo.ComponentFactory#check(org.apache.felix.ipojo.metadata.Element)
+     */
+    public boolean check(Element cm) {
+        String name = cm.getAttribute("name");
+        if (name == null) {
+            System.err.println("A composite needs a name");
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public String getClassName() { return "composite"; }
+        
+    
+    /**
+     * Compute required handlers.
+     */
+    public List getRequiredHandlerList() {
+        List list = new ArrayList();
+        Element[] elems = m_componentMetadata.getElements();
+        for (int i = 0; i < elems.length; i++) {
+            Element current = elems[i]; 
+            RequiredHandler hi = new RequiredHandler(current.getName(), current.getNameSpace());
+            if (! list.contains(hi)) { list.add(hi); }
+        }
+        
+        // Add architecture if architecture != 'false'
+        String arch = m_componentMetadata.getAttribute("architecture");
+        if (arch == null || arch.equalsIgnoreCase("true")) {
+            RequiredHandler hi = new RequiredHandler("architecture", null);
+            if (! list.contains(hi)) { list.add(hi); }
+        }
+        
+        return list;
+    }
+    
+    /**
+     * Stop all the instance managers.
+     */
+    public synchronized void stopping() {
+        if (m_tracker != null) {
+            m_tracker.close();
+        }
+        m_tracker = null;
+    }
+
+    /**
+     * Start all the instance managers.
+     */
+    public synchronized void starting() {
+        if (m_requiredHandlers.size() != 0) {
+            try {
+                String filter = "(&(" + Constants.OBJECTCLASS + "=" + Factory.class.getName() + ")"
+                    + "(" + Handler.HANDLER_TYPE_PROPERTY + "=" + CompositeHandler.HANDLER_TYPE + ")" 
+                    + "(factory.state=1)"
+                    + ")";
+                m_tracker = new Tracker(m_context, m_context.createFilter(filter), this);
+                m_tracker.open();
+            } catch (InvalidSyntaxException e) {
+                m_logger.log(Logger.ERROR, "A factory filter is not valid: " + e.getMessage());
+                stop();
+                return;
+            }
+        }
+    }
+    
+    public ComponentInstance createInstance(Dictionary configuration, IPojoContext context, HandlerManager[] handlers) throws ConfigurationException {
+        CompositeManager inst = new CompositeManager(this, context, handlers);
+        inst.configure(m_componentMetadata, configuration);
+        inst.start();
+        return inst;
+    }
+
+    /**
+     * Reconfigure an existing instance.
+     * @param properties : the new configuration to push.
+     * @throws UnacceptableConfiguration : occurs if the new configuration is
+     * not consistent with the component type.
+     * @throws MissingHandlerException : occurs when an handler is unavailable when creating the instance.
+     * @see org.apache.felix.ipojo.Factory#reconfigure(java.util.Dictionary)
+     */
+    public synchronized void reconfigure(Dictionary properties) throws UnacceptableConfiguration, MissingHandlerException {
+        if (properties == null || properties.get("name") == null) {
+            throw new UnacceptableConfiguration("The configuration does not contains the \"name\" property");
+        }
+        final String name = (String) properties.get("name");
+        
+        ComponentInstance cm = (CompositeManager) m_componentInstances.get(name);
+        
+        if (cm == null) {
+            return; // The instance does not exist.
+        }
+        
+        cm.reconfigure(properties); // re-configure the component
+    }
+
+    public String getFactoryName() {
+        return m_componentMetadata.getAttribute("name");
+    }
+
+}
---
+++ src/main/java/org/apache/felix/ipojo/composite/service/instantiator/ServiceInstantiatorHandler.java
@@ -0,0 +1,156 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.composite.service.instantiator;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.felix.ipojo.ConfigurationException;
+import org.apache.felix.ipojo.architecture.HandlerDescription;
+import org.apache.felix.ipojo.composite.CompositeHandler;
+import org.apache.felix.ipojo.metadata.Element;
+
+/**
+ * Service Instantiator Class. This handler allows to instantiate service
+ * instance inside the composition.
+ * 
+ * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+ */
+public class ServiceInstantiatorHandler extends CompositeHandler {
+
+    /**
+     * List of instances to manage.
+     */
+    private List/* <SvcInstance> */m_instances = new ArrayList();
+
+    /**
+     * Configure the handler.
+     * 
+     * @param metadata : the metadata of the component
+     * @param conf : the instance configuration
+     * @throws ConfigurationException : the specification attribute is missing
+     * @see org.apache.felix.ipojo.CompositeHandler#configure(org.apache.felix.ipojo.CompositeManager,
+     * org.apache.felix.ipojo.metadata.Element, java.util.Dictionary)
+     */
+    public void configure(Element metadata, Dictionary conf) throws ConfigurationException {
+        Element[] services = metadata.getElements("service");
+        for (int i = 0; i < services.length; i++) {
+            String spec = services[i].getAttribute("specification");
+            if (spec == null) {
+                throw new ConfigurationException("Malformed service : the specification attribute is mandatory");
+            }
+            String filter = "(&(!(factory.name=" + getCompositeManager().getFactory().getComponentDescription().getName() + "))(factory.state=1))"; // Cannot reinstantiate yourself
+            String f = services[i].getAttribute("filter");
+            if (f != null) {
+                filter = "(&" + filter + f + ")";
+            }
+            Properties prop = new Properties();
+            for (int k = 0; k < services[i].getElements("property").length; k++) {
+                String key = services[i].getElements("property")[k].getAttribute("name");
+                String value = services[i].getElements("property")[k].getAttribute("value");
+                prop.put(key, value);
+            }
+            String ag = services[i].getAttribute("aggregate");
+            boolean agg = ag != null && ag.equalsIgnoreCase("true");
+            
+            String op = services[i].getAttribute("optional");
+            boolean opt = op != null && op.equalsIgnoreCase("true");
+            
+            SvcInstance inst = new SvcInstance(this, spec, prop, agg, opt, filter);
+            m_instances.add(inst);
+        }
+    }
+
+    /**
+     * Start the service instantiator handler.
+     * Start all created service instance.
+     * @see org.apache.felix.ipojo.CompositeHandler#start()
+     */
+    public void start() {
+        // Init
+        for (int i = 0; i < m_instances.size(); i++) {
+            SvcInstance inst = (SvcInstance) m_instances.get(i);
+            inst.start();
+        }
+
+        isHandlerValid();
+    }
+
+    /**
+     * Check the handler validity.
+     * @return true if all created service instances are valid
+     * @see org.apache.felix.ipojo.CompositeHandler#isValid()
+     */
+    private void isHandlerValid() {
+        for (int i = 0; i < m_instances.size(); i++) {
+            SvcInstance inst = (SvcInstance) m_instances.get(i);
+            if (!inst.isSatisfied()) {
+                setValidity(false);
+                return;
+            }
+        }
+        setValidity(true);
+    }
+
+    /**
+     * Handler stop method.
+     * Stop all created service instance.
+     * @see org.apache.felix.ipojo.CompositeHandler#stop()
+     */
+    public void stop() {
+        for (int i = 0; i < m_instances.size(); i++) {
+            SvcInstance inst = (SvcInstance) m_instances.get(i);
+            inst.stop();
+        }
+        m_instances.clear();
+    }
+
+    /**
+     * An service instance becomes valid.
+     */
+    public void validate() {
+        if (!getValidity()) {
+            isHandlerValid();
+        }
+    }
+
+    /**
+     * A service instance becomes invalid.
+     */
+    public void invalidate() {
+        if (getValidity()) {
+            isHandlerValid();
+        }
+    }
+
+    /**
+     * Get the service instantiator handler description.
+     * @return the description
+     * @see org.apache.felix.ipojo.CompositeHandler#getDescription()
+     */
+    public HandlerDescription getDescription() {
+        return new ServiceInstantiatorDescription(this, m_instances);
+    }
+    
+    public List getInstances() {
+        return m_instances;
+    }
+}
