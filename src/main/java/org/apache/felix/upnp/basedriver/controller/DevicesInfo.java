ec46bd79a02c6284b2510af807ab02b5c7a2f178
Francesco Furfari <furfari@apache.org>
2008-01-02 15:30:40 +0000

    Modified dependency graph for: - basedriver; dependency with extra bundle is not more needed. Now the basedriver directly exports the interfaces of the services it registers with the framework (DriverController.java and DevicesInfo.java) - extra bundle provides only common utilities to write OSGi/UPnP Device services. It is used by the examples - tv, clock and bynarylight use the extra packages (except for UPnpSubscriber; to do later) - tester use the basedriver packages for forcing MSearch and Debugging and for resolving  relative URLs
    Added thread name to TVFrame.class and ClockFrame.class
    
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/upnp/basedriver@608127 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/felix/upnp/basedriver/controller/DevicesInfo.java
@@ -0,0 +1,57 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.felix.upnp.basedriver.controller;
+
+/* 
+* @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+*/
+public interface DevicesInfo{
+    /**
+     * 
+     * Allow you to get the URL that poinr to the XML description of
+     * a device specified by UUID. 
+     * 
+     * @param udn the UUID that identify a device
+     * @return The String that rappresent the URL that point to the description of the Device
+     */
+    public String getLocationURL(String udn);
+    
+    /**
+     * 
+     * Allow you to get the URL that poinr to the XML description of
+     * a service specified by ServiceId and UUID of the device that 
+     * contain the service 
+     * 
+     * @param udn the UUID of the device that contain the service
+     * @param serviceId the ServiceId of the service
+     * @return The String that rappresent the URL that point to the description of the Service
+     */
+    public String getSCPDURL(String udn,String serviceId);  
+    
+    /**
+     * Allow you to get the absolue URL of a link that is conatin in a device
+     * 
+     * @param udn the UUID of the UPnP Device 
+     * @param link the relative link that you want to resolve
+     * @return The String that rappresent the absolute URL to the resourse specified by link
+     */
+    public String resolveRelativeUrl(String udn, String link);
+    
+}
---
+++ src/main/java/org/apache/felix/upnp/basedriver/util/Constants.java
@@ -0,0 +1,32 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.felix.upnp.basedriver.util;
+/* 
+* @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+*/
+public interface Constants {
+	/**
+	 * If this property is set on a UPnP Device means that the 
+	 * device service is been created by UPnP base Driver. <br>
+	 * The value of the does not carry any mean. <br>
+	 * The name of the property is "UPnP.device.import".
+	 */
+	public static final String UPNP_IMPORT = "UPnP.device.imported"; 
+}
