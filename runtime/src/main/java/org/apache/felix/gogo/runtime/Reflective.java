3f86ffec4bdbcb6e3277367c5caaa9c3efa08df1
Derek Baum <dbaum@apache.org>
2010-06-21 12:37:57 +0000

    fix NPE/coercion error when passing null first argument FELIX-2432
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/gogo@956547 13f79535-47bb-0310-9956-ffa450edef68

--- runtime/src/main/java/org/apache/felix/gogo/runtime/Reflective.java
+++ runtime/src/main/java/org/apache/felix/gogo/runtime/Reflective.java
@@ -171,7 +171,7 @@ public final class Reflective
                 {
                     params.append(", ");
                 }
-                params.append(arg.getClass().getSimpleName());
+                params.append(arg == null ? "null" : arg.getClass().getSimpleName());
             }
 
             throw new IllegalArgumentException(String.format(
@@ -275,6 +275,13 @@ public final class Reflective
                 else
                 {
                     out[i] = coerce(session, target, types[i], in.get(0));
+                    
+                    if (out[i] == null && types[i].isArray() && in.size() > 0)
+                    {
+                        // don't coerce null to array FELIX-2432
+                        out[i] = NO_MATCH;
+                    }
+                    
                     if (out[i] != NO_MATCH)
                     {
                         in.remove(0);
