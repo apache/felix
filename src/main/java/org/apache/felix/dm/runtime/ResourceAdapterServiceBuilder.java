e070aa3e05740c4294f4e9ea17848d5a2e95ba38
Marcel Offermans <marrs@apache.org>
2010-07-01 14:01:02 +0000

    Used a Java 5 style for() loop by accident. Extended the signature for creating resource adapters so you can also specify a callback instance and name for the changed method.
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/dependencymanager/runtime@959668 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/dm/runtime/ResourceAdapterServiceBuilder.java
+++ src/main/java/org/apache/felix/dm/runtime/ResourceAdapterServiceBuilder.java
@@ -44,7 +44,7 @@ public class ResourceAdapterServiceBuilder extends ServiceComponentBuilder
         String[] service = srvMeta.getStrings(Params.service, null);
         Dictionary<String, Object> properties = srvMeta.getDictionary(Params.properties, null);
         boolean propagate = "true".equals(srvMeta.getString(Params.propagate, "false"));
-        Service srv = dm.createResourceAdapterService(filter, propagate)
+        Service srv = dm.createResourceAdapterService(filter, propagate, null, "changed")
                         .setInterface(service, properties);       
         String factoryMethod = srvMeta.getString(Params.factoryMethod, null);
         if (factoryMethod == null)
