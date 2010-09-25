08dd501238484ec03e8fa82f6114bf8375f35a65
Clement Escoffier <clement@apache.org>
2010-09-25 09:42:09 +0000

    Fix FELIX-2622 Support static service properties that are not mirrored into fields Introduce a new StaticServiceProperty annotation: @Component @Provides( 		specifications= {FooService.class, BarService.class}, 		properties= { 				@StaticServiceProperty(name="prop1", value="prop1", type="java.lang.String"), 				@StaticServiceProperty(name="prop2", type="java.lang.String"), 				@StaticServiceProperty(name="props", value="{prop1, prop2}", type="string[]"), 				@StaticServiceProperty(name="mandatory1", mandatory=true, type="string") 		})
    To improve the error management, we can't use the ServiceProperty annotation as the type attribute is mandatory.		
    
    
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/ipojo/annotations@1001175 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/ipojo/annotations/Provides.java
+++ src/main/java/org/apache/felix/ipojo/annotations/Provides.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -27,16 +27,16 @@ import java.lang.annotation.Target;
 */
 @Target(ElementType.TYPE)
 public @interface Provides {
-    
+
     /**
      * Set the provided specifications.
      * Default : all implemented interfaces
      */
     Class[] specifications() default { };
-    
+
     /**
      * Set the service object creation strategy.
-     * Two value are possible: SINGLETON, SERVICE, METHOD, INSTANCE or the strategy class name. 
+     * Two value are possible: SINGLETON, SERVICE, METHOD, INSTANCE or the strategy class name.
      * SERVICE means OSGi Service Factory.
      * METHOD delegates the creation to the factory-method of the component
      * INSTANCE creates one service object per requiring instance
@@ -44,4 +44,14 @@ public @interface Provides {
      * Default : SINGLETON
      */
     String strategy() default "SINGLETON";
+
+    /**
+     * Allows adding static properties to the service.
+     * Nested properties are static service properties,
+     * so <b>must</b> contain the name and the value as
+     * they are not attached to a field.
+     * The array contains {@link StaticServiceProperty} elements.
+     * Default : No service properties
+     */
+    StaticServiceProperty[] properties() default {};
 }
