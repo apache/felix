ee2e028555d3b35d2666c3c73fd9777d9171b3f1
Felix Meschberger <fmeschbe@apache.org>
2010-10-21 11:01:04 +0000

    FELIX-2669 Fix dependencies and ensure build is using Java 5 source/target for annotation and generics support
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/scrplugin/generator@1025941 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/scrplugin/tags/annotation/AbstractTag.java
+++ src/main/java/org/apache/felix/scrplugin/tags/annotation/AbstractTag.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.felix.scrplugin.tags.annotation.defaulttag;
+package org.apache.felix.scrplugin.tags.annotation;
 
 import java.util.Map;
 
