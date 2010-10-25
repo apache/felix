dd67e32695f4b4f6fe9d5496a81a2df58ea4754d
Guillaume Nodet <gnodet@apache.org>
2010-10-25 20:19:04 +0000

    [FELIX-2571] Have fileinstall listen for configuration changes and write them back to the config files
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/utils@1027254 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/felix/utils/properties/InterpolationHelperTest.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.felix.utils.properties;
+
+import junit.framework.TestCase;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+public class InterpolationHelperTest extends TestCase {
+
+    public void testBasicSubstitution()
+    {
+        System.setProperty("value1", "sub_value1");
+        Hashtable props = new Hashtable();
+        props.put("key0", "value0");
+        props.put("key1", "${value1}");
+        props.put("key2", "${value2}");
+
+        for (Enumeration e = props.keys(); e.hasMoreElements();)
+        {
+            String name = (String) e.nextElement();
+            props.put(name, InterpolationHelper.substVars((String) props.get(name), name, null, props));
+        }
+
+        assertEquals("value0", props.get("key0"));
+        assertEquals("sub_value1", props.get("key1"));
+        assertEquals("", props.get("key2"));
+
+    }
+
+    public void testSubstitutionFailures()
+    {
+        assertEquals("a}", InterpolationHelper.substVars("a}", "b", null, new Hashtable()));
+        assertEquals("${a", InterpolationHelper.substVars("${a", "b", null, new Hashtable()));
+    }
+
+    public void testEmptyVariable() {
+        assertEquals("", InterpolationHelper.substVars("${}", "b", null, new Hashtable()));
+    }
+
+    public void testInnerSubst() {
+        Hashtable props = new Hashtable();
+        props.put("a", "b");
+        props.put("b", "c");
+        assertEquals("c", InterpolationHelper.substVars("${${a}}", "z", null, props));
+    }
+
+    public void testSubstLoop() {
+        try {
+            InterpolationHelper.substVars("${a}", "a", null, new Hashtable());
+            fail("Should have thrown an exception");
+        } catch (IllegalArgumentException e) {
+            // expected
+        }
+    }
+
+    public void testSubstitutionEscape()
+    {
+        assertEquals("${a}", InterpolationHelper.substVars("$\\{a${#}\\}", "b", null, new Hashtable()));
+        assertEquals("${a}", InterpolationHelper.substVars("$\\{a\\}${#}", "b", null, new Hashtable()));
+        assertEquals("${a}", InterpolationHelper.substVars("$\\{a\\}", "b", null, new Hashtable()));
+    }
+
+}
