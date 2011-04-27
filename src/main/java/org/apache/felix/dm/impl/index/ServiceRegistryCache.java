8d4d77ce1427f8117fc2a23f8fa2455e930c5370
Marcel Offermans <marrs@apache.org>
2011-04-27 08:00:29 +0000

    Moved the FilterIndex to the public package. Although highly experimental, it is part of the API.
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/dependencymanager/core@1097029 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/dm/impl/index/ServiceRegistryCache.java
+++ src/main/java/org/apache/felix/dm/impl/index/ServiceRegistryCache.java
@@ -24,6 +24,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import org.apache.felix.dm.FilterIndex;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
