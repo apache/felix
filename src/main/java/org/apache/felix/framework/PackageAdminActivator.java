30ef8c1b585b87bdc12f3ae5901e1ee7c35099b3
Richard S. Hall <rickhall@apache.org>
2011-05-23 17:49:43 +0000

    Initial pass at implementing FrameworkWiringImpl. More stress testing is likely needed, but most of the code was simply stolen from the related PackageAdmin implementation. (FELIX-2969)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/framework@1126616 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/framework/PackageAdminActivator.java
+++ src/main/java/org/apache/felix/framework/PackageAdminActivator.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -42,6 +42,5 @@ class PackageAdminActivator implements BundleActivator
     public void stop(BundleContext context) throws Exception
     {
         m_reg.unregister();
-        m_packageAdmin.stop();
     }
 }
--- src/main/java/org/apache/felix/framework/PackageAdminImpl.java
+++ src/main/java/org/apache/felix/framework/PackageAdminImpl.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -27,12 +27,13 @@ import org.osgi.framework.Constants;
 import org.osgi.framework.Version;
 import org.osgi.framework.wiring.BundleRevision;
 import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.FrameworkWiring;
 import org.osgi.service.packageadmin.ExportedPackage;
 import org.osgi.service.packageadmin.PackageAdmin;
 import org.osgi.service.packageadmin.RequiredBundle;
 
 
-class PackageAdminImpl implements PackageAdmin, Runnable
+class PackageAdminImpl implements PackageAdmin
 {
     private static final Comparator COMPARATOR = new Comparator() {
         public int compare(Object o1, Object o2)
@@ -44,48 +45,16 @@ class PackageAdminImpl implements PackageAdmin, Runnable
     };
 
     private Felix m_felix = null;
-    private Bundle[][] m_reqBundles = null;
-    private Bundle m_systemBundle = null;
-    private Thread m_thread = null;
 
     public PackageAdminImpl(Felix felix)
     {
         m_felix = felix;
-        m_systemBundle = m_felix.getBundle(0);
-
-        // Start a thread to perform asynchronous package refreshes.
-        m_thread = new Thread(this, "FelixPackageAdmin");
-        m_thread.setDaemon(true);
-        m_thread.start();
     }
 
-    /**
-     * Stops the FelixPackageAdmin thread on system shutdown. Shutting down the
-     * thread explicitly is required in the embedded case, where Felix may be
-     * stopped without the Java VM being stopped. In this case the
-     * FelixPackageAdmin thread must be stopped explicitly.
-     * <p>
-     * This method is called by the
-     * {@link PackageAdminActivator#stop(BundleContext)} method.
-     */
-    synchronized void stop()
-    {
-        if (m_thread != null)
-        {
-            // Null thread variable to signal to the thread that
-            // we want it to exit.
-            m_thread = null;
-            
-            // Wake up the thread, if it is currently in the wait() state
-            // for more work.
-            notifyAll();
-        }
-    }
-    
     /**
      * Returns the bundle associated with this class if the class was
      * loaded from a bundle, otherwise returns null.
-     * 
+     *
      * @param clazz the class for which to determine its associated bundle.
      * @return the bundle associated with the specified class, otherwise null.
     **/
@@ -99,7 +68,7 @@ class PackageAdminImpl implements PackageAdmin, Runnable
      * version is in the specified version range. If no version range is
      * specified, then all bundles with the specified symbolic name are
      * returned. The array is sorted in descending version order.
-     * 
+     *
      * @param symbolicName the target symbolic name.
      * @param versionRange the target version range.
      * @return an array of matching bundles sorted in descending version order.
@@ -121,7 +90,7 @@ class PackageAdminImpl implements PackageAdmin, Runnable
                 }
             }
         }
-        if (list.size() == 0)
+        if (list.isEmpty())
         {
             return null;
         }
@@ -279,102 +248,17 @@ class PackageAdminImpl implements PackageAdmin, Runnable
     public void refreshPackages(Bundle[] bundles)
         throws SecurityException
     {
-        Object sm = System.getSecurityManager();
-        
-        if (sm != null)
-        {
-            ((SecurityManager) sm).checkPermission(
-                new AdminPermission(m_systemBundle, AdminPermission.RESOLVE));
-        }
-        synchronized (this)
-        {
-            // Save our request parameters and notify all.
-            if (m_reqBundles == null)
-            {
-                m_reqBundles = new Bundle[][] { bundles };
-            }
-            else
-            {
-                Bundle[][] newReqBundles = new Bundle[m_reqBundles.length + 1][];
-                System.arraycopy(m_reqBundles, 0,
-                    newReqBundles, 0, m_reqBundles.length);
-                newReqBundles[m_reqBundles.length] = bundles;
-                m_reqBundles = newReqBundles;
-            }
-            notifyAll();
-        }
+        List<Bundle> list = (bundles == null)
+            ? null
+            : Arrays.asList(bundles);
+        m_felix.adapt(FrameworkWiring.class).refreshBundles(list);
     }
 
     public boolean resolveBundles(Bundle[] bundles)
     {
-        Object sm = System.getSecurityManager();
-        
-        if (sm != null)
-        {
-            ((SecurityManager) sm).checkPermission(
-                new AdminPermission(m_systemBundle, AdminPermission.RESOLVE));
-        }
-        
-        return m_felix.resolveBundles(bundles);
-    }
-
-    /**
-     * The OSGi specification states that package refreshes happen
-     * asynchronously; this is the run() method for the package
-     * refreshing thread.
-    **/
-    public void run()
-    {
-        // This thread loops forever, thus it should
-        // be a daemon thread.
-        while (true)
-        {
-            Bundle[] bundles = null;
-            synchronized (this)
-            {
-                // Wait for a refresh request.
-                while (m_reqBundles == null)
-                {
-                    // Terminate the thread if requested to do so (see stop()).
-                    if (m_thread == null)
-                    {
-                        return;
-                    }
-                    
-                    try
-                    {
-                        wait();
-                    }
-                    catch (InterruptedException ex)
-                    {
-                    }
-                }
-
-                // Get the bundles parameter for the current refresh request.
-                bundles = m_reqBundles[0];
-            }
-
-            // Perform refresh.
-            // NOTE: We don't catch any exceptions here, because
-            // the invoked method shields us from exceptions by
-            // catching Throwables when its invokes callbacks.
-            m_felix.refreshPackages(bundles);
-
-            // Remove the first request since it is now completed.
-            synchronized (this)
-            {
-                if (m_reqBundles.length == 1)
-                {
-                    m_reqBundles = null;
-                }
-                else
-                {
-                    Bundle[][] newReqBundles = new Bundle[m_reqBundles.length - 1][];
-                    System.arraycopy(m_reqBundles, 1,
-                        newReqBundles, 0, m_reqBundles.length - 1);
-                    m_reqBundles = newReqBundles;
-                }
-            }
-        }
+        List<Bundle> list = (bundles == null)
+            ? null
+            : Arrays.asList(bundles);
+        return m_felix.adapt(FrameworkWiring.class).resolveBundles(list);
     }
 }
