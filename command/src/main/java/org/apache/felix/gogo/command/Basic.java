b0cb64d3392281c3e6e5cc93df854ba00d4a7584
Richard S. Hall <rickhall@apache.org>
2011-08-22 21:26:54 +0000

    Initial pass at implementing a consistent "inspect" command for pre/post OSGi R4.3. (FELIX-3050)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/gogo@1160444 13f79535-47bb-0310-9956-ffa450edef68

--- command/src/main/java/org/apache/felix/gogo/command/Basic.java
+++ command/src/main/java/org/apache/felix/gogo/command/Basic.java
@@ -414,13 +414,22 @@ public class Basic
         return commands;
     }
 
-    @Descriptor("inspects bundle dependency information")
-    public void inspect(
-        @Descriptor("(package | bundle | fragment | service)") String type,
-        @Descriptor("(capability | requirement)") String direction,
+    @Descriptor("inspects bundle capabilities and requirements")
+    public void in(
+        @Descriptor("('capability' | 'requirement')") String direction,
+        @Descriptor("(<namespace> | 'service')") String namespace,
         @Descriptor("target bundles") Bundle[] bundles)
     {
-        Inspect.inspect(m_bc, type, direction, bundles);
+        Inspect.in(m_bc, direction, namespace, bundles);
+    }
+
+    @Descriptor("inspects bundle capabilities and requirements")
+    public void inspect(
+        @Descriptor("('capability' | 'requirement')") String direction,
+        @Descriptor("('package' | 'bundle' | 'host' | 'service')") String namespace,
+        @Descriptor("target bundles") Bundle[] bundles)
+    {
+        Inspect.inspect(m_bc, direction, namespace, bundles);
     }
 
     @Descriptor("install bundle using URLs")
