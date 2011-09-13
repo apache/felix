ec867c24af41e1134d6dc5c9769f897dbcdb71f5
Valentin Valchev <vvalchev@apache.org>
2011-09-13 05:58:32 +0000

    Really fixed FELIX-2889 : Invalid JSON content in http response after starting or stopping a bundle.
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1170053 13f79535-47bb-0310-9956-ffa450edef68

--- webconsole/src/main/java/org/apache/felix/webconsole/internal/core/BundlesServlet.java
+++ webconsole/src/main/java/org/apache/felix/webconsole/internal/core/BundlesServlet.java
@@ -355,8 +355,8 @@ public class BundlesServlet extends SimpleWebConsolePlugin implements OsgiManage
                 // write the state only
                 resp.setContentType( "application/json" ); //$NON-NLS-1$
                 resp.setCharacterEncoding( "UTF-8" ); //$NON-NLS-1$
-                resp.getWriter().print("{fragment:" + isFragmentBundle(bundle) // //$NON-NLS-1$
-                    + ",stateRaw:" + bundle.getState() + "}"); //$NON-NLS-1$ //$NON-NLS-2$
+                resp.getWriter().print("{\"fragment\":" + isFragmentBundle(bundle) // //$NON-NLS-1$
+                    + ",\"stateRaw\":" + bundle.getState() + "}"); //$NON-NLS-1$ //$NON-NLS-2$
                 return;
             }
         }
