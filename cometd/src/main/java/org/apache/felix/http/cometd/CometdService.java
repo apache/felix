f83917879e4434d7a6ab78fb9a0a238a84e0984d
Felix Meschberger <fmeschbe@apache.org>
2011-09-23 13:38:57 +0000

    FELIX-3102 Applied the patch supplied by Julian Sedding(thanks)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/http@1174750 13f79535-47bb-0310-9956-ffa450edef68

--- cometd/src/main/java/org/apache/felix/http/cometd/CometdService.java
+++ cometd/src/main/java/org/apache/felix/http/cometd/CometdService.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.felix.http.api;
+package org.apache.felix.http.cometd;
 
 import javax.servlet.ServletException;
 import org.cometd.Bayeux;
