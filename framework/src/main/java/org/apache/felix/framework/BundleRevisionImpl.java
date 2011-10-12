b979ff1a00d35a9a2b8ff4336cd34707caf9cad1
Rob Walker <walkerr@apache.org>
2011-10-12 13:41:32 +0000

    Null pointer check in close method (FELIX-3160)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1182359 13f79535-47bb-0310-9956-ffa450edef68

--- framework/src/main/java/org/apache/felix/framework/BundleRevisionImpl.java
+++ framework/src/main/java/org/apache/felix/framework/BundleRevisionImpl.java
@@ -636,7 +636,12 @@ public class BundleRevisionImpl implements BundleRevision
             ((BundleImpl) m_bundle).getFramework().getLogger().log(
                 Logger.LOG_ERROR, "Error releasing revision: " + ex.getMessage(), ex);
         }
-        m_content.close();
+        
+        if (m_content != null)
+        {
+            m_content.close();
+        }
+        
         m_content = null;
         for (int i = 0; (m_contentPath != null) && (i < m_contentPath.size()); i++)
         {
