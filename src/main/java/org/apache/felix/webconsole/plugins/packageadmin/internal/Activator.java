79465a65aef8ad52ae302fa29c00e4d9be4b5c01
Felix Meschberger <fmeschbe@apache.org>
2011-10-21 19:09:09 +0000

    Reformat according to Apache Felix conventions
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/webconsole-plugins/packageadmin@1187509 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/webconsole/plugins/packageadmin/internal/Activator.java
+++ src/main/java/org/apache/felix/webconsole/plugins/packageadmin/internal/Activator.java
@@ -1,18 +1,18 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -24,7 +24,8 @@ import java.util.Hashtable;
 import org.osgi.framework.*;
 import org.osgi.util.tracker.ServiceTracker;
 
-public class Activator implements BundleActivator {
+public class Activator implements BundleActivator
+{
 
     private ServiceTracker pkgAdminTracker;
 
@@ -32,46 +33,49 @@ public class Activator implements BundleActivator {
 
     private ServiceRegistration depFinderPlugin;
 
-    public void start(final BundleContext context) throws Exception {
-        this.pkgAdminTracker = new ServiceTracker(context,
-            "org.osgi.service.packageadmin.PackageAdmin", null);
+    public void start(final BundleContext context) throws Exception
+    {
+        this.pkgAdminTracker = new ServiceTracker(context, "org.osgi.service.packageadmin.PackageAdmin", null);
         this.pkgAdminTracker.open();
 
         registerPackageAdminPlugin(context);
         registerDependencyFinderPlugin(context);
     }
 
-    private void registerPackageAdminPlugin(final BundleContext context) {
-        final PackageAdminPlugin plugin = new PackageAdminPlugin(context,
-            pkgAdminTracker);
+    private void registerPackageAdminPlugin(final BundleContext context)
+    {
+        final PackageAdminPlugin plugin = new PackageAdminPlugin(context, pkgAdminTracker);
         final Dictionary<String, Object> props = new Hashtable<String, Object>();
         props.put("felix.webconsole.label", PackageAdminPlugin.LABEL);
         props.put("felix.webconsole.title", PackageAdminPlugin.TITLE);
-        props.put("felix.webconsole.configprinter.modes", new String[] {"zip", "txt"});
-        this.pkgAdminPlugin = context.registerService("javax.servlet.Servlet",
-            plugin, props);
+        props.put("felix.webconsole.configprinter.modes", new String[]
+            { "zip", "txt" });
+        this.pkgAdminPlugin = context.registerService("javax.servlet.Servlet", plugin, props);
     }
 
-    private void registerDependencyFinderPlugin(final BundleContext context) {
-        final DependencyFinderPlugin plugin = new DependencyFinderPlugin(context,
-            pkgAdminTracker);
+    private void registerDependencyFinderPlugin(final BundleContext context)
+    {
+        final DependencyFinderPlugin plugin = new DependencyFinderPlugin(context, pkgAdminTracker);
         final Dictionary<String, Object> props = new Hashtable<String, Object>();
         props.put("felix.webconsole.label", DependencyFinderPlugin.LABEL);
         props.put("felix.webconsole.title", DependencyFinderPlugin.TITLE);
-        this.depFinderPlugin = context.registerService("javax.servlet.Servlet",
-            plugin, props);
+        this.depFinderPlugin = context.registerService("javax.servlet.Servlet", plugin, props);
     }
 
-    public void stop(final BundleContext context) throws Exception {
-        if ( this.pkgAdminPlugin != null ) {
+    public void stop(final BundleContext context) throws Exception
+    {
+        if (this.pkgAdminPlugin != null)
+        {
             this.pkgAdminPlugin.unregister();
             this.pkgAdminPlugin = null;
         }
-        if ( this.depFinderPlugin != null ) {
+        if (this.depFinderPlugin != null)
+        {
             this.depFinderPlugin.unregister();
             this.depFinderPlugin = null;
         }
-        if ( this.pkgAdminTracker != null ) {
+        if (this.pkgAdminTracker != null)
+        {
             this.pkgAdminTracker.close();
             this.pkgAdminTracker = null;
         }
--- src/main/java/org/apache/felix/webconsole/plugins/packageadmin/internal/PackageAdminPlugin.java
+++ src/main/java/org/apache/felix/webconsole/plugins/packageadmin/internal/PackageAdminPlugin.java
@@ -1,18 +1,18 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
+ * KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
@@ -33,7 +33,8 @@ import org.osgi.service.packageadmin.PackageAdmin;
 import org.osgi.util.tracker.ServiceTracker;
 
 @SuppressWarnings("serial")
-public class PackageAdminPlugin extends GenericServlet {
+public class PackageAdminPlugin extends GenericServlet
+{
 
     static final String LABEL = "pkgadmin";
 
@@ -45,47 +46,52 @@ public class PackageAdminPlugin extends GenericServlet {
 
     private final ServiceTracker pkgAdminTracker;
 
-    PackageAdminPlugin(final BundleContext bundleContext,
-            final ServiceTracker pkgAdminTracker) {
+    PackageAdminPlugin(final BundleContext bundleContext, final ServiceTracker pkgAdminTracker)
+    {
         this.bundleContext = bundleContext;
         this.pkgAdminTracker = pkgAdminTracker;
     }
 
-    public void service(ServletRequest req, ServletResponse res)
-            throws IOException {
+    public void service(ServletRequest req, ServletResponse res) throws IOException
+    {
 
         final PrintWriter pw = ((HttpServletResponse) res).getWriter();
 
         final PackageAdmin pa = (PackageAdmin) this.pkgAdminTracker.getService();
-        if (pa == null) {
+        if (pa == null)
+        {
             printStatLine(pw, "PackageAdmin Service not registered");
             return;
         }
 
-        try {
+        try
+        {
             Map<String, Set<ExportedPackage>> exports = collectExportedPackages(pa);
-            printStatLine(pw,
-                "PackageAdmin service reports %d exported packages",
-                exports.size());
+            printStatLine(pw, "PackageAdmin service reports %d exported packages", exports.size());
             dumpDuplicates(pw, exports);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             pw.println("failure ...." + e);
             e.printStackTrace(pw);
         }
     }
 
-    private Map<String, Set<ExportedPackage>> collectExportedPackages(
-            final PackageAdmin pa) {
+    private Map<String, Set<ExportedPackage>> collectExportedPackages(final PackageAdmin pa)
+    {
         Map<String, Set<ExportedPackage>> exports = new TreeMap<String, Set<ExportedPackage>>();
 
-        for (Bundle bundle : this.bundleContext.getBundles()) {
+        for (Bundle bundle : this.bundleContext.getBundles())
+        {
             final ExportedPackage[] bundleExports = pa.getExportedPackages(bundle);
-            if (bundleExports != null) {
-                for (ExportedPackage exportedPackage : bundleExports) {
+            if (bundleExports != null)
+            {
+                for (ExportedPackage exportedPackage : bundleExports)
+                {
                     Set<ExportedPackage> exportSet = exports.get(exportedPackage.getName());
-                    if (exportSet == null) {
-                        exportSet = new TreeSet<ExportedPackage>(
-                            EXPORT_PACKAGE_COMPARATOR);
+                    if (exportSet == null)
+                    {
+                        exportSet = new TreeSet<ExportedPackage>(EXPORT_PACKAGE_COMPARATOR);
                         exports.put(exportedPackage.getName(), exportSet);
                     }
                     exportSet.add(exportedPackage);
@@ -96,39 +102,44 @@ public class PackageAdminPlugin extends GenericServlet {
         return exports;
     }
 
-    private void printStatLine(final PrintWriter pw, final String format,
-            final Object... args) {
+    private void printStatLine(final PrintWriter pw, final String format, final Object... args)
+    {
         final String message = String.format(format, args);
         pw.printf("<p class=\"statline\">%s</p>%n", message);
     }
 
-    private void printTitle(final PrintWriter pw, final String format,
-            final Object... args) {
+    private void printTitle(final PrintWriter pw, final String format, final Object... args)
+    {
         pw.println("<div class=\"ui-widget-header ui-corner-top\">");
         pw.printf(format, args);
         pw.println("</div>");
     }
 
-    private void dumpDuplicates(final PrintWriter pw,
-            final Map<String, Set<ExportedPackage>> exports) {
+    private void dumpDuplicates(final PrintWriter pw, final Map<String, Set<ExportedPackage>> exports)
+    {
         printTitle(pw, "Duplicate Exported Packages");
         pw.println("<table class=\"nicetable\">");
         pw.println("<tr><th>Package</th><th>Exports</th><th>Imports</th></tr>");
-        for (Entry<String, Set<ExportedPackage>> exportEntry : exports.entrySet()) {
+        for (Entry<String, Set<ExportedPackage>> exportEntry : exports.entrySet())
+        {
             Set<ExportedPackage> exportSet = exportEntry.getValue();
-            if (exportSet.size() > 1) {
-                String firstCol = String.format("<td rowspan=\"%s\">%s</td>",
-                    exportSet.size(), exportEntry.getKey());
-                for (ExportedPackage exportedPackage : exportSet) {
+            if (exportSet.size() > 1)
+            {
+                String firstCol = String.format("<td rowspan=\"%s\">%s</td>", exportSet.size(), exportEntry.getKey());
+                for (ExportedPackage exportedPackage : exportSet)
+                {
                     Bundle[] importers = exportedPackage.getImportingBundles();
-                    pw.printf("<tr>%s<td>version=%s, Bundle %s</td><td>",
-                        firstCol, exportedPackage.getVersion(),
+                    pw.printf("<tr>%s<td>version=%s, Bundle %s</td><td>", firstCol, exportedPackage.getVersion(),
                         exportedPackage.getExportingBundle());
-                    if (importers != null && importers.length > 0) {
-                        for (Bundle bundle : importers) {
+                    if (importers != null && importers.length > 0)
+                    {
+                        for (Bundle bundle : importers)
+                        {
                             pw.printf("%s<br>", bundle);
                         }
-                    } else {
+                    }
+                    else
+                    {
                         pw.print("&nbsp;");
                     }
                     pw.println("</td></tr>");
@@ -139,13 +150,13 @@ public class PackageAdminPlugin extends GenericServlet {
         pw.println("</table>");
     }
 
-    private void dumpDuplicatesAsTxt(final PrintWriter pw,
-            final Map<String, Set<ExportedPackage>> exports)
+    private void dumpDuplicatesAsTxt(final PrintWriter pw, final Map<String, Set<ExportedPackage>> exports)
     {
         pw.println("Duplicate Exported Packages");
         pw.println("---------------------------");
         final List<String[]> lines = new ArrayList<String[]>();
-        lines.add(new String[] {"Package", "Exports", "Imports"});
+        lines.add(new String[]
+            { "Package", "Exports", "Imports" });
 
         for (Entry<String, Set<ExportedPackage>> exportEntry : exports.entrySet())
         {
@@ -156,46 +167,51 @@ public class PackageAdminPlugin extends GenericServlet {
                 for (ExportedPackage exportedPackage : exportSet)
                 {
                     final Bundle[] importers = exportedPackage.getImportingBundles();
-                    final String secondCol = "version=" + exportedPackage.getVersion() + ", Bundle " + exportedPackage.getExportingBundle();
-                    if (importers != null && importers.length > 0) {
+                    final String secondCol = "version=" + exportedPackage.getVersion() + ", Bundle "
+                        + exportedPackage.getExportingBundle();
+                    if (importers != null && importers.length > 0)
+                    {
                         boolean first = true;
                         for (Bundle bundle : importers)
                         {
-                            if ( first )
+                            if (first)
                             {
-                                lines.add(new String[] {firstCol, secondCol, bundle.toString()});
+                                lines.add(new String[]
+                                    { firstCol, secondCol, bundle.toString() });
                                 first = false;
                             }
                             else
                             {
-                                lines.add(new String[] {"", "", bundle.toString()});
+                                lines.add(new String[]
+                                    { "", "", bundle.toString() });
 
                             }
                         }
                     }
                     else
                     {
-                        lines.add(new String[] {firstCol, secondCol, ""});
+                        lines.add(new String[]
+                            { firstCol, secondCol, "" });
                     }
                     firstCol = "";
                 }
             }
         }
         int maxFirst = 0, maxSecond = 0;
-        for(final String[] entry : lines)
+        for (final String[] entry : lines)
         {
-            if ( entry[0].length() > maxFirst )
+            if (entry[0].length() > maxFirst)
             {
                 maxFirst = entry[0].length();
             }
-            if ( entry[1].length() > maxSecond )
+            if (entry[1].length() > maxSecond)
             {
                 maxSecond = entry[1].length();
             }
         }
         maxFirst += 2;
         maxSecond += 2;
-        for(final String[] entry : lines)
+        for (final String[] entry : lines)
         {
             padText(pw, entry[0], maxFirst);
             padText(pw, entry[1], maxSecond);
@@ -207,31 +223,35 @@ public class PackageAdminPlugin extends GenericServlet {
     {
         pw.print(text);
         final int padLength = length - text.length();
-        for(int i=0;i<padLength;i++) {
+        for (int i = 0; i < padLength; i++)
+        {
             pw.print(' ');
         }
     }
 
-    private static class ExportedPackageComparator implements
-            Comparator<ExportedPackage> {
+    private static class ExportedPackageComparator implements Comparator<ExportedPackage>
+    {
 
-        public int compare(ExportedPackage o1, ExportedPackage o2) {
-            if (o1 == o2) {
+        public int compare(ExportedPackage o1, ExportedPackage o2)
+        {
+            if (o1 == o2)
+            {
                 return 0;
             }
 
             int name = o1.getName().compareTo(o2.getName());
-            if (name != 0) {
+            if (name != 0)
+            {
                 return name;
             }
 
             int version = o1.getVersion().compareTo(o2.getVersion());
-            if (version != 0) {
+            if (version != 0)
+            {
                 return version;
             }
 
-            return Long.valueOf(o1.getExportingBundle().getBundleId()).compareTo(
-                o2.getExportingBundle().getBundleId());
+            return Long.valueOf(o1.getExportingBundle().getBundleId()).compareTo(o2.getExportingBundle().getBundleId());
         }
 
     }
@@ -248,7 +268,8 @@ public class PackageAdminPlugin extends GenericServlet {
             return;
         }
 
-        try {
+        try
+        {
             Map<String, Set<ExportedPackage>> exports = collectExportedPackages(pa);
 
             pw.print("PackageAdmin service reports ");
@@ -257,7 +278,9 @@ public class PackageAdminPlugin extends GenericServlet {
             pw.println();
 
             dumpDuplicatesAsTxt(pw, exports);
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             pw.println("failure ...." + e);
         }
     }
