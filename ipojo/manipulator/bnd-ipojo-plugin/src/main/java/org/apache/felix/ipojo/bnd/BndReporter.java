f0a7db9f0d32512b8cc52327880d2795842617ae
Clement Escoffier <clement@apache.org>
2011-12-03 09:36:25 +0000

    New manipulator structure - Move the bnd ipojo plugin
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1209867 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ipojo/manipulator/bnd-ipojo-plugin/src/main/java/org/apache/felix/ipojo/bnd/BndReporter.java
@@ -0,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.felix.ipojo.bnd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import aQute.libg.reporter.Reporter;
+import org.apache.felix.ipojo.manipulator.reporter.EmptyReporter;
+
+/**
+ * A {@code BndReporter} knows how to wrap a Bnd Reporter into an iPOJO Reporter.
+ *
+ * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+ */
+public class BndReporter extends EmptyReporter {
+
+    /**
+     * Bnd reporter.
+     */
+    private Reporter m_reporter;
+
+    /**
+     * Errors which occur during the manipulation.
+     */
+    private List<String> m_errors = new ArrayList<String>();
+
+    /**
+     * Warnings which occur during the manipulation.
+     */
+    private List<String> m_warnings = new ArrayList<String>();
+
+    public BndReporter(aQute.libg.reporter.Reporter reporter) {
+        m_reporter = reporter;
+    }
+
+    public List<String> getErrors() {
+        return m_errors;
+    }
+
+    public List<String> getWarnings() {
+        return m_warnings;
+    }
+
+    @Override
+    public void trace(String message, Object... args) {
+        m_reporter.trace(message, args);
+    }
+
+    @Override
+    public void info(String message, Object... args) {
+        m_reporter.progress(message, args);
+    }
+
+    @Override
+    public void warn(String message, Object... args) {
+        m_reporter.warning(message, args);
+        m_warnings.add(String.format(message, args));
+    }
+
+    @Override
+    public void error(String message, Object... args) {
+        m_reporter.error(message, args);
+        m_errors.add(String.format(message, args));
+    }
+}
---
+++ ipojo/manipulator/bnd-ipojo-plugin/src/main/java/org/apache/felix/ipojo/bnd/ResourceMetadataProvider.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.bnd;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import aQute.lib.osgi.Resource;
+import org.apache.felix.ipojo.manipulator.MetadataProvider;
+import org.apache.felix.ipojo.manipulator.Reporter;
+import org.apache.felix.ipojo.manipulator.metadata.StreamMetadataProvider;
+import org.apache.felix.ipojo.metadata.Element;
+
+/**
+ * A {@code ResourceMetadataProvider} is ...
+ *
+ * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+ */
+public class ResourceMetadataProvider implements MetadataProvider {
+
+    private Resource m_resource;
+
+    private List<Element> m_cached;
+
+    private boolean m_validateUsingLocalSchemas = false;
+
+    private Reporter m_reporter;
+
+    public ResourceMetadataProvider(Resource resource, Reporter reporter) {
+        m_resource = resource;
+        m_reporter = reporter;
+    }
+
+    public void setValidateUsingLocalSchemas(boolean validateUsingLocalSchemas) {
+        this.m_validateUsingLocalSchemas = validateUsingLocalSchemas;
+    }
+
+    public List<Element> getMetadatas() throws IOException {
+        if (m_cached == null) {
+            m_cached = new ArrayList<Element>();
+            InputStream stream = null;
+            try {
+                stream = m_resource.openInputStream();
+            } catch (Exception e) {
+                m_reporter.error(e.getMessage());
+                throw new IOException("Cannot read metadata");
+            }
+            StreamMetadataProvider provider = new StreamMetadataProvider(stream, m_reporter);
+            provider.setValidateUsingLocalSchemas(m_validateUsingLocalSchemas);
+            m_cached.addAll(provider.getMetadatas());
+        }
+
+        return m_cached;
+    }
+}
