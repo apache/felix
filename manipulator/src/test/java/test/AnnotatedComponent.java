f17743c489b2a1670d7acf7965e37f1530cf8beb
Clement Escoffier <clement@apache.org>
2011-12-06 15:04:44 +0000

    Prepare for the next release: * Added missing license header * Updatee changelogs * Fixed build * Added LICENSE, NOTICE and DEPENDENCIES files
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/ipojo/manipulator@1210959 13f79535-47bb-0310-9956-ffa450edef68

--- manipulator/src/test/java/test/AnnotatedComponent.java
+++ manipulator/src/test/java/test/AnnotatedComponent.java
@@ -1,15 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package test;
 
 import org.apache.felix.ipojo.annotations.Component;
 import org.apache.felix.ipojo.annotations.Property;
 
-/**
- * Created by IntelliJ IDEA.
- * User: guillaume
- * Date: 10/08/11
- * Time: 21:56
- * To change this template use File | Settings | File Templates.
- */
 @Component
 public class AnnotatedComponent {
 
