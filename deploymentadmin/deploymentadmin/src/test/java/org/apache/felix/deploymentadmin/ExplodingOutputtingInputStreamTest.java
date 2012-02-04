b8340510a1b82e7d720d6def117d20867f70a7ee
Marcel Offermans <marrs@apache.org>
2012-02-04 13:22:02 +0000

    FELIX-3336 Finalized the patch after finally understanding exactly what was going on.
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1240500 13f79535-47bb-0310-9956-ffa450edef68

--- deploymentadmin/deploymentadmin/src/test/java/org/apache/felix/deploymentadmin/ExplodingOutputtingInputStreamTest.java
+++ deploymentadmin/deploymentadmin/src/test/java/org/apache/felix/deploymentadmin/ExplodingOutputtingInputStreamTest.java
@@ -1,8 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.felix.deploymentadmin;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -54,6 +75,65 @@ public class ExplodingOutputtingInputStreamTest extends TestCase {
         
         // cleanup
         zipfile.delete();
+        index.delete();
+        d1.delete();
+        d2.delete();
+        dir.delete();
+        tempDir.delete();
+    }
+    
+    public void testStreamReadWithJARStream() throws Exception {
+        // fill up a stringbuffer with some test data
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < 1000; i++) {
+            sb.append("DATAdataDATAdata");
+        }
+        String data = sb.toString();
+        
+        // create a temporary folder
+        File tempDir = File.createTempFile("temp", "dir");
+        tempDir.delete();
+        tempDir.mkdirs();
+        System.out.println("Dir: " + tempDir);
+        
+        // create a zip file with two entries in it
+        File jarfile = new File(tempDir, "jarfile");
+        JarOutputStream jos = new JarOutputStream(new FileOutputStream(jarfile));
+        String dummy1 = "dummy";
+        jos.putNextEntry(new JarEntry(dummy1));
+        jos.write(data.getBytes());
+        jos.closeEntry();
+        String dummy2 = "dummy2";
+        jos.putNextEntry(new JarEntry(dummy2));
+        jos.write(data.getBytes());
+        jos.closeEntry();
+        jos.close();
+        
+        // create another temporary folder
+        File dir = new File(tempDir, "dir");
+        dir.mkdirs();
+        File index = new File(tempDir, "list");
+        ExplodingOutputtingInputStream stream = new ExplodingOutputtingInputStream(new FileInputStream(jarfile), index, dir);
+        JarInputStream jarInputStream = new JarInputStream(stream);
+
+        JarEntry entry;
+        while ((entry = jarInputStream.getNextJarEntry()) != null) {
+            int size = 0;
+            byte[] buffer = new byte[4096];
+            for (int i = jarInputStream.read(buffer); i > -1; i = jarInputStream.read(buffer)) {
+                size += i;
+            }
+            System.out.println("read JAR entry: " + entry + " of " + size + " bytes.");
+            jarInputStream.closeEntry();
+        }
+        stream.close();
+        
+        // create references to the unpacked dummy files
+        File d1 = new File(dir, dummy1);
+        File d2 = new File(dir, dummy2);
+        
+        // cleanup
+        jarfile.delete();
         index.delete();
         d1.delete();
         d2.delete();
