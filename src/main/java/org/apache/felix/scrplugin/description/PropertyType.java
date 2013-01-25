9f1cd2e83a502f12204b4e312cf1c9cc8da138c2
Carsten Ziegeler <cziegeler@apache.org>
2013-01-25 10:01:52 +0000

    FELIX-3169 :  Add support for new PASSWORD attribute type of Metatype service
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/scrplugin/generator@1438410 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/scrplugin/description/PropertyType.java
+++ src/main/java/org/apache/felix/scrplugin/description/PropertyType.java
@@ -29,7 +29,8 @@ public enum PropertyType {
     Char,
     Character,
     Boolean,
-    Short;
+    Short,
+    Passwort;
 
     public static PropertyType from(final Class<?> javaClass) {
         if ( javaClass.getName().equals(Long.class.getName())) {
