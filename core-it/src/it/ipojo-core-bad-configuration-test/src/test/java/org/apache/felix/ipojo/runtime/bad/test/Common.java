2a9939b4a8624164809bec64c603baecf01ef186
Clement Escoffier <clement@apache.org>
2013-02-14 15:40:03 +0000

    Migrate some core tests (bad configuration) to pax exam 3
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/ipojo/runtime@1446244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-it/src/it/ipojo-core-bad-configuration-test/src/test/java/org/apache/felix/ipojo/runtime/bad/test/Common.java
@@ -0,0 +1,146 @@
+package org.apache.felix.ipojo.runtime.bad.test;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.CompositeOption;
+import org.ops4j.pax.exam.options.DefaultCompositeOption;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.tinybundles.core.TinyBundle;
+import org.ops4j.pax.tinybundles.core.TinyBundles;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.ow2.chameleon.testing.helpers.IPOJOHelper;
+import org.ow2.chameleon.testing.helpers.OSGiHelper;
+import org.ow2.chameleon.testing.tinybundles.ipojo.IPOJOStrategy;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+/**
+ * Bootstrap the test from this project
+ */
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class Common {
+
+    @Inject
+    BundleContext bc;
+
+    OSGiHelper osgiHelper;
+    IPOJOHelper ipojoHelper;
+
+    Bundle testedBundle;
+
+    @Configuration
+    public Option[] config() throws MalformedURLException {
+        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.INFO);
+
+        return options(
+                ipojoBundles(),
+                junitBundles(),
+                testedBundle(),
+                systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN")
+        );
+    }
+
+    @Before
+    public void commonSetUp() {
+        osgiHelper = new OSGiHelper(bc);
+        ipojoHelper = new IPOJOHelper(bc);
+
+        testedBundle = osgiHelper.getBundle("test.bundle");
+
+        // Dump OSGi Framework information
+        String vendor = (String) osgiHelper.getBundle(0).getHeaders().get(Constants.BUNDLE_VENDOR);
+        if (vendor == null) {
+            vendor = (String) osgiHelper.getBundle(0).getHeaders().get(Constants.BUNDLE_SYMBOLICNAME);
+        }
+        String version = (String) osgiHelper.getBundle(0).getHeaders().get(Constants.BUNDLE_VERSION);
+        System.out.println("OSGi Framework : " + vendor + " - " + version);
+    }
+
+    @After
+    public void commonTearDown() {
+        ipojoHelper.dispose();
+        osgiHelper.dispose();
+    }
+
+    public CompositeOption ipojoBundles() {
+        return new DefaultCompositeOption(
+                mavenBundle("org.apache.felix", "org.apache.felix.ipojo").versionAsInProject(),
+                mavenBundle("org.ow2.chameleon.testing", "osgi-helpers").versionAsInProject());
+    }
+
+    public Option testedBundle() throws MalformedURLException {
+        File out = new File("target/tested/bundle.jar");
+
+        TinyBundle tested = TinyBundles.bundle();
+
+        // We look inside target/classes to find the class and resources
+        File classes = new File("target/classes");
+        Collection<File> files = FileUtils.listFilesAndDirs(classes, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+        List<File> services = new ArrayList<File>();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                // By convention we export of .services and .service package
+                if (file.getName().endsWith("services") || file.getName().endsWith("service")) {
+                    services.add(file);
+                }
+            } else {
+                // We need to compute the path
+                String path = file.getAbsolutePath().substring(classes.getAbsolutePath().length() + 1);
+                tested.add(path, file.toURI().toURL());
+                System.out.println(file.getName() + " added to " + path);
+            }
+        }
+
+        String export = "";
+        for (File file : services) {
+            if (export.length() > 0) {
+                export += ", ";
+            }
+            String path = file.getAbsolutePath().substring(classes.getAbsolutePath().length() + 1);
+            String packageName = path.replace('/', '.');
+            export += packageName;
+        }
+
+        System.out.println("Exported packages : " + export);
+
+        InputStream inputStream = tested
+                .set(Constants.BUNDLE_SYMBOLICNAME, "test.bundle")
+                .set(Constants.IMPORT_PACKAGE, "*")
+                .set(Constants.EXPORT_PACKAGE, export)
+                .build(IPOJOStrategy.withiPOJO(new File("src/main/resources")));
+
+        try {
+            FileUtils.copyInputStreamToFile(inputStream, out);
+            return bundle(out.toURI().toURL().toExternalForm());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Cannot compute the url of the manipulated bundle");
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot write of the manipulated bundle");
+        }
+    }
+
+
+}
---
+++ core-it/src/it/ipojo-core-bad-configuration-test/src/test/java/org/apache/felix/ipojo/runtime/bad/test/TestBadLFCCallback.java
@@ -0,0 +1,261 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.runtime.bad.test;
+
+import org.apache.felix.ipojo.*;
+import org.apache.felix.ipojo.metadata.Attribute;
+import org.apache.felix.ipojo.metadata.Element;
+import org.apache.felix.ipojo.parser.ManifestMetadataParser;
+import org.apache.felix.ipojo.parser.ParseException;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+public class TestBadLFCCallback extends Common {
+
+    private String clazz = "org.apache.felix.ipojo.runtime.bad.components.CallbackCheckService";
+    private String type = "BAD-CallbackCheckService";
+    private Element manipulation;
+    private Properties props;
+
+    @Before
+    public void setUp() {
+        manipulation = getManipulationForComponent();
+        props = new Properties();
+        props.put("instance.name", "BAD");
+    }
+
+
+    private Element getNothing() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getNoTransition() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        callback.addAttribute(new Attribute("method", "start"));
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getNoMethod() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        callback.addAttribute(new Attribute("transition", "validate"));
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getBadMethod() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        callback.addAttribute(new Attribute("transition", "validate"));
+        callback.addAttribute(new Attribute("method", "start_")); // Missing method.
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getBadMethod2() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        callback.addAttribute(new Attribute("transition", "invalidate"));
+        callback.addAttribute(new Attribute("method", "stop_")); // Missing method.
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getBadTransition() {
+        Element elem = new Element("component", "");
+        elem.addAttribute(new Attribute("classname", clazz));
+
+        Element callback = new Element("callback", "");
+        callback.addAttribute(new Attribute("method", "start"));
+        callback.addAttribute(new Attribute("transition", "validate_"));
+        elem.addElement(callback);
+        elem.addElement(manipulation);
+        return elem;
+    }
+
+    private Element getManipulationForComponent() {
+        String header = testedBundle.getHeaders().get("iPOJO-Components");
+        Element elem = null;
+        try {
+            elem = ManifestMetadataParser.parseHeaderMetadata(header);
+        } catch (ParseException e) {
+            fail("Parse Exception when parsing iPOJO-Component");
+        }
+
+        assertNotNull("Check elem not null", elem);
+
+        Element manip = getManipulationForComponent(elem, type);
+        assertNotNull("Check manipulation metadata not null for " + type, manip);
+        return manip;
+    }
+
+    private Element getManipulationForComponent(Element metadata, String comp_name) {
+        Element[] comps = metadata.getElements("component");
+        for (int i = 0; i < comps.length; i++) {
+            if (comps[i].containsAttribute("factory") && comps[i].getAttribute("factory").equals(comp_name)) {
+                return comps[i].getElements("manipulation")[0];
+            }
+            if (comps[i].containsAttribute("name") && comps[i].getAttribute("name").equals(comp_name)) {
+                return comps[i].getElements("manipulation")[0];
+            }
+        }
+        return null;
+    }
+
+    @Test
+    public void testNothing() {
+        try {
+            ComponentFactory cf = new ComponentFactory(osgiHelper.getContext(), getNothing());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            ci.dispose();
+            cf.stop();
+            fail("A lifecycle callback with a missing method and transition must be rejected " + cf);
+        } catch (ConfigurationException e) {
+            // OK
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testNoTransition() {
+        try {
+            ComponentFactory cf = new ComponentFactory(osgiHelper.getContext(), getNoTransition());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            ci.dispose();
+            cf.stop();
+            fail("A lifecycle callback with a missing transition must be rejected " + cf);
+        } catch (ConfigurationException e) {
+            // OK
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testNoMethod() {
+        try {
+            ComponentFactory cf = new ComponentFactory(osgiHelper.getContext(), getNoMethod());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            ci.dispose();
+            cf.stop();
+            fail("A lifecycle callback with a missing method must be rejected " + cf);
+        } catch (ConfigurationException e) {
+            // OK
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBadMethod() {
+        try {
+            ComponentFactory cf = new ComponentFactory(testedBundle.getBundleContext(), getBadMethod());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            if (ci.isStarted()) {
+                fail("A lifecycle callback with a bad method must be rejected (instance stills valid)" + cf);
+            }
+            ci.dispose();
+            cf.stop();
+        } catch (ConfigurationException e) {
+            //The check does not happen in the configure method.
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBadMethod2() {
+        try {
+            ComponentFactory cf = new ComponentFactory(testedBundle.getBundleContext(), getBadMethod2());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            ci.stop();
+            if (ci.isStarted()) {
+                fail("A lifecycle callback with a bad method must be rejected (instance stills valid)" + cf);
+            }
+            ci.dispose();
+            cf.stop();
+        } catch (ConfigurationException e) {
+            //The check does not happen in the configure method.
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testBadTransition() {
+        try {
+            ComponentFactory cf = new ComponentFactory(osgiHelper.getContext(), getBadTransition());
+            cf.start();
+            ComponentInstance ci = cf.createComponentInstance(props);
+            ci.dispose();
+            cf.stop();
+            fail("A lifecycle callback with a bad transition must be rejected " + cf);
+        } catch (ConfigurationException e) {
+            // OK
+        } catch (UnacceptableConfiguration e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        } catch (MissingHandlerException e) {
+            fail("Unexpected exception when creating an instance : " + e.getMessage());
+        }
+    }
+
+}
