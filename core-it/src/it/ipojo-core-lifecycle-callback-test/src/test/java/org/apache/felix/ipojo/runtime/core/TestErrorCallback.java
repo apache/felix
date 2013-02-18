252ddcdf7f3fa839ac742e57bc5fcebbba9c866e
Clement Escoffier <clement@apache.org>
2013-02-18 21:05:35 +0000

    FELIX-3903
    Migrate the core lifecycle callback tests to pax exam 3
    
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/ipojo/runtime@1447493 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core-it/src/it/ipojo-core-lifecycle-callback-test/src/test/java/org/apache/felix/ipojo/runtime/core/TestErrorCallback.java
@@ -0,0 +1,60 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.runtime.core;
+
+import org.apache.felix.ipojo.ComponentInstance;
+import org.apache.felix.ipojo.runtime.core.services.CheckService;
+import org.junit.After;
+import org.junit.Test;
+import org.osgi.framework.ServiceReference;
+
+import java.util.Properties;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Test the fix for FELIX-1965.
+ * When a validate callback throws an exception, the service is still provided.
+ */
+public class TestErrorCallback extends Common {
+    
+    ComponentInstance instance; // Instance under test
+
+    @After
+    public void tearDown() {
+        if (instance != null) {
+            instance.dispose();
+            instance= null;
+        }
+    }
+    
+    @Test
+    public void testErrorInValidateCallback() {
+        Properties p2 = new Properties();
+        p2.put("instance.name","error");
+        instance = ipojoHelper.createComponentInstance("LFCB-CallbackWithError", p2);
+        
+        // The service should not be provided as the start method has thrown an exception
+        ServiceReference ref = osgiHelper.getServiceReference(CheckService.class.getName(),
+                "(instance.name=" + instance.getInstanceName() + ")");
+        assertNull(ref);
+    }
+        
+
+}
---
+++ core-it/src/it/ipojo-core-lifecycle-callback-test/src/test/java/org/apache/felix/ipojo/runtime/core/TestImmediateLifeCycleController.java
@@ -0,0 +1,114 @@
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.ipojo.runtime.core;
+
+import org.apache.felix.ipojo.ComponentInstance;
+import org.apache.felix.ipojo.Factory;
+import org.apache.felix.ipojo.runtime.core.services.CheckService;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.ServiceReference;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class TestImmediateLifeCycleController extends Common {
+
+    private ComponentInstance under;
+    private Factory factory;
+
+    @Before
+    public void setUp() {
+        factory = ipojoHelper.getFactory("LFC-Test-Immediate");
+    }
+
+    @Test
+    public void testOne() {
+        Properties props = new Properties();
+        props.put("conf", "foo");
+        props.put("instance.name", "under");
+        under = ipojoHelper.createComponentInstance("LFC-Test-Immediate", props);
+
+        // The conf is correct, the PS must be provided
+        ServiceReference ref = ipojoHelper.getServiceReferenceByName(CheckService.class.getName(), "under");
+        assertNotNull("Check service availability -1", ref);
+        CheckService cs = (CheckService) osgiHelper.getServiceObject(ref);
+        assertTrue("Check state 1", cs.check());
+        bc.ungetService(ref);
+
+        // Reconfigure the instance with a bad configuration
+        props.put("conf", "bar"); // Bar is a bad conf
+        try {
+            factory.reconfigure(props);
+        } catch (Exception e) {
+            fail("The reconfiguration is not unacceptable and seems unacceptable : " + props);
+        }
+
+        // The instance should now be invalid 
+        ref = ipojoHelper.getServiceReferenceByName(CheckService.class.getName(), "under");
+        assertNull("Check service availability -2", ref);
+
+        // Reconfigure the instance with a valid configuration
+        props.put("conf", "foo"); // Bar is a bad conf
+        try {
+            factory.reconfigure(props);
+        } catch (Exception e) {
+            fail("The reconfiguration is not unacceptable and seems unacceptable (2) : " + props);
+        }
+
+        ref = ipojoHelper.getServiceReferenceByName(CheckService.class.getName(), "under");
+        assertNotNull("Check service availability -3", ref);
+        cs = (CheckService) osgiHelper.getServiceObject(ref);
+        assertTrue("Check state 2", cs.check());
+        bc.ungetService(ref);
+        under.dispose();
+    }
+
+    @Test
+    public void testTwo() {
+        Properties props = new Properties();
+        props.put("conf", "bar");
+        props.put("instance.name", "under");
+        under = ipojoHelper.createComponentInstance("LFC-Test-Immediate", props);
+
+        assertEquals("check under state", under.getState(), ComponentInstance.INVALID);
+
+        // The conf is incorrect, the PS must not be provided
+        ServiceReference ref = ipojoHelper.getServiceReferenceByName(CheckService.class.getName(), "under");
+        assertNull("Check service availability -1", ref);
+
+        // Reconfigure the instance with a correct configuration
+        props.put("conf", "foo");
+        try {
+            factory.reconfigure(props);
+        } catch (Exception e) {
+            fail("The reconfiguration is not unacceptable and seems unacceptable : " + props);
+        }
+
+        ref = ipojoHelper.getServiceReferenceByName(CheckService.class.getName(), "under");
+        assertNotNull("Check service availability -2", ref);
+        CheckService cs = (CheckService) osgiHelper.getServiceObject(ref);
+        assertTrue("Check state ", cs.check());
+        bc.ungetService(ref);
+        under.dispose();
+    }
+
+
+}
