a87cbf7edfa08f538630958c4814f8627cbd8cf3
Carsten Ziegeler <cziegeler@apache.org>
2013-02-27 07:41:48 +0000

    Minor javadoc update
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/inventory@1450660 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/inventory/InventoryPrinter.java
+++ src/main/java/org/apache/felix/inventory/InventoryPrinter.java
@@ -31,6 +31,7 @@ import java.io.PrintWriter;
  *   <li>{@link #CONFIG_TITLE} - the printer title</li>
  *   <li>{@link #CONFIG_NAME} - the printer name</li>
  * </ul>
+ *
  */
 public interface InventoryPrinter {
 
@@ -54,7 +55,7 @@ public interface InventoryPrinter {
      * The unique name of the printer.
      * If this property is missing the printer is ignored.
      * If there are two or more services with the same name, the
-     * services with the highest ranking is used.
+     * service with the highest ranking is used.
      */
     String CONFIG_NAME = "felix.inventory.printer.name"; //$NON-NLS-1$
 
@@ -69,6 +70,7 @@ public interface InventoryPrinter {
      * Optional property controlling whether the printer will be displayed
      * in the web console. By default, a printer is displayed in the
      * web console, unless this property is added with the value 'false'.
+     * The property value can either be a boolean or a string.
      */
     String CONFIG_WEBCONSOLE = "felix.inventory.printer.webconsole"; //$NON-NLS-1$
 
@@ -79,10 +81,15 @@ public interface InventoryPrinter {
      * If a printer is invoked with a mode it doesn't support ({@link #CONFIG_PRINTER_MODES})
      * the printer should just do/print nothing and directly return.
      *
+     * A printer might be used in one of two different situations: either for
+     * directly displaying the information to a user (like in the web console)
+     * or the output is included in a ZIP. The printer might want to return
+     * different output depending on the usage situation.
+     *
      * @param mode The render mode.
      * @param printWriter where to write the configuration data. It might be flushed,
      * but must not be closed.
-     * @param isZip whether this is included in a zip file or used directly
+     * @param isZip whether this is included in a ZIP file or used directly
      */
     void print( PrinterMode mode, PrintWriter printWriter, boolean isZip );
 }
