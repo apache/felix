9932017ece80252acda068bbdae46bf6537ebb79
Felix Meschberger <fmeschbe@apache.org>
2013-03-04 08:09:29 +0000

    Add toString method and remove superfluous (IMO) comment references
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/inventory@1452204 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/inventory/PrinterMode.java
+++ src/main/java/org/apache/felix/inventory/PrinterMode.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -62,17 +62,11 @@ public final class PrinterMode
         return this.mode;
     }
 
-    /**
-     * @see java.lang.Object#hashCode()
-     */
     public int hashCode()
     {
         return mode.hashCode();
     }
 
-    /**
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
     public boolean equals(Object obj)
     {
         if (this == obj)
@@ -89,5 +83,10 @@ public final class PrinterMode
         }
         final PrinterMode other = (PrinterMode) obj;
         return mode.equals(other.mode);
+    }
+
+    public String toString()
+    {
+        return name();
     }
 }
