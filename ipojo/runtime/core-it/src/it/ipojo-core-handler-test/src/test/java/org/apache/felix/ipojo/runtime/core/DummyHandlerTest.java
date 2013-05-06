076175a7c836154c7e0ace9b940768ef4f0e7252
Clement Escoffier <clement@apache.org>
2013-05-06 09:13:45 +0000

    fix test failure on equinox
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1479502 13f79535-47bb-0310-9956-ffa450edef68

--- ipojo/runtime/core-it/src/it/ipojo-core-handler-test/src/test/java/org/apache/felix/ipojo/runtime/core/DummyHandlerTest.java
+++ ipojo/runtime/core-it/src/it/ipojo-core-handler-test/src/test/java/org/apache/felix/ipojo/runtime/core/DummyHandlerTest.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -25,8 +25,8 @@ import org.apache.felix.ipojo.*;
 import org.apache.felix.ipojo.runtime.core.components.DummyImpl;
 import org.apache.felix.ipojo.runtime.core.handlers.DummyHandler;
 import org.apache.felix.ipojo.runtime.core.services.Dummy;
+import org.apache.felix.ipojo.runtime.core.services.User;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.OptionUtils;
@@ -35,7 +35,7 @@ import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.ops4j.pax.tinybundles.core.TinyBundles;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.useradmin.User;
+import org.ow2.chameleon.testing.helpers.TimeUtils;
 import org.ow2.chameleon.testing.tinybundles.ipojo.IPOJOStrategy;
 
 import java.io.File;
@@ -43,9 +43,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.*;
 import static org.ops4j.pax.exam.CoreOptions.bundle;
 import static org.ops4j.pax.exam.CoreOptions.streamBundle;
 import static org.ops4j.pax.tinybundles.core.TinyBundles.withBnd;
@@ -86,6 +84,7 @@ public class DummyHandlerTest extends Common {
         return OptionUtils.combine(options,
                 streamBundle(TinyBundles.bundle()
                         .add(Dummy.class)
+                        .add(User.class)
                         .set(Constants.EXPORT_PACKAGE, "org.apache.felix.ipojo.runtime.core.services")
                         .set(Constants.BUNDLE_SYMBOLICNAME, "service")
                         .build(withBnd())
@@ -138,31 +137,30 @@ public class DummyHandlerTest extends Common {
         Factory factory = Tools.getValidFactory(osgiHelper, DUMMY_TEST_FACTORY);
         assertNotNull(factory);
 
-        // Create an instance
+        // Create an instance, it will be disposed by the helper
         ComponentInstance instance = ipojoHelper.createComponentInstance(DUMMY_TEST_FACTORY);
+        assertTrue(instance.getState() == ComponentInstance.VALID);
 
-        Map<User, ServiceRegistration> registrations = new HashMap<User, ServiceRegistration>();
+        Map<MyUser, ServiceRegistration> registrations = new HashMap<MyUser, ServiceRegistration>();
 
         for (int i = 0; i < NB_MOCK; i++) {
-            User service = mock(User.class);
+            MyUser service = new MyUser();
             ServiceRegistration sr = bc.registerService(User.class.getName(), service, null);
             registrations.put(service, sr);
         }
 
+        TimeUtils.grace(200);
+        assertEquals(osgiHelper.getServiceReferences(User.class, null).length, NB_MOCK);
+
         //verify that the bind method of the handler has been called
-        for (User user : registrations.keySet()) {
-            verify(user).getName();
+        for (MyUser user : registrations.keySet()) {
+            assertTrue(user.name);
         }
 
         //verify that the unbind has been called
-        for (User user : registrations.keySet()) {
+        for (MyUser user : registrations.keySet()) {
             registrations.get(user).unregister();
-            verify(user).getType();
-        }
-
-        //verify no more interaction
-        for (User user : registrations.keySet()) {
-            Mockito.verifyNoMoreInteractions(user);
+            assertTrue(user.type);
         }
     }
 
@@ -173,10 +171,10 @@ public class DummyHandlerTest extends Common {
     public void testDummyTestBindBeforeStart() {
         ComponentInstance instance = null;
 
-        Map<User, ServiceRegistration> registrations = new HashMap<User, ServiceRegistration>();
+        Map<MyUser, ServiceRegistration> registrations = new HashMap<MyUser, ServiceRegistration>();
 
         for (int i = 0; i < NB_MOCK; i++) {
-            User service = mock(User.class);
+            MyUser service = new MyUser();
             ServiceRegistration sr = bc.registerService(User.class.getName(), service, null);
             registrations.put(service, sr);
         }
@@ -185,22 +183,36 @@ public class DummyHandlerTest extends Common {
         Factory factory = Tools.getValidFactory(osgiHelper, DUMMY_TEST_FACTORY);
         assertNotNull(factory);
 
-        instance = ipojoHelper.createComponentInstance(DUMMY_TEST_FACTORY);
+        // The instance will be disposed by the helper
+        ipojoHelper.createComponentInstance(DUMMY_TEST_FACTORY);
 
         //verify that the bind method of the handler has been called
-        for (User user : registrations.keySet()) {
-            verify(user).getName();
+        for (MyUser user : registrations.keySet()) {
+            assertTrue(user.name);
         }
 
         //verify that the unbind has been called
-        for (User user : registrations.keySet()) {
+        for (MyUser user : registrations.keySet()) {
             registrations.get(user).unregister();
-            verify(user).getType();
+            assertTrue(user.type);
+        }
+    }
+
+    private class MyUser implements User {
+
+        boolean name;
+        boolean type;
+
+        @Override
+        public String getName() {
+            name = true;
+            return "name";
         }
 
-        //verify no more interaction
-        for (User user : registrations.keySet()) {
-            Mockito.verifyNoMoreInteractions(user);
+        @Override
+        public int getType() {
+            type = true;
+            return 1;
         }
     }
 }
