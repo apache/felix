f1935b5344e9534863a879d67f0fb0485eb92aac
Clement Escoffier <clement@apache.org>
2013-07-02 16:58:45 +0000

    Added the 10 minutes tutorial to the runtime project (FELIX-4133 Add distribution creation in the iPOJO runtime build)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1499015 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ipojo/runtime/distributions/ten-minutes-tutorial/src/main/solution/spell.english/src/spell/english/EnglishDictionary.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package spell.english;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+import spell.services.DictionaryService;
+
+/**
+ * An implementation of the Dictionary service containing English words
+ * see DictionaryService for details of the service.
+ **/
+@Component // It's an iPOJO Component
+@Provides // We provide a service
+@Instantiate // We declare an instance of our component
+public class EnglishDictionary implements DictionaryService {
+
+    // The set of words contained in the dictionary.
+    String[] dictionary = { "welcome", "to", "the", "ipojo", "tutorial" };
+
+    /**
+     * Implements DictionaryService.checkWord(). Determines
+     * if the passed in word is contained in the dictionary.
+     * @param word the word to be checked.
+     * @return true if the word is in the dictionary,
+     *         false otherwise.
+     **/
+    public boolean checkWord(String word) {
+        word = word.toLowerCase();
+
+        // This is very inefficient
+        for (String dict : dictionary) {
+            if (dict.equals(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
