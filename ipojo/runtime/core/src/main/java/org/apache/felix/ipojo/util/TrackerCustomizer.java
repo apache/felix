6c08d6dd6e1a2821c25b433ed3152c4b88d1ca09
Clement Escoffier <clement@apache.org>
2013-08-21 09:16:40 +0000

    Fix FELIX-4183
    Fix the very outdated javadoc.
    
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1516117 13f79535-47bb-0310-9956-ffa450edef68

--- ipojo/runtime/core/src/main/java/org/apache/felix/ipojo/util/TrackerCustomizer.java
+++ ipojo/runtime/core/src/main/java/org/apache/felix/ipojo/util/TrackerCustomizer.java
@@ -28,32 +28,40 @@ public interface TrackerCustomizer {
 
     /**
      * A service is being added to the Tracker object.
-     * This method is called before a service which matched the search parameters of the Tracker object is added to it. This method should return the service object to be tracked for this ServiceReference object.
-     * The returned service object is stored in the Tracker object and is available from the getService and getServices methods.
+     * This method is called before a service which matched the search parameters of the Tracker object is added to
+     * it. This method should return the service object to be tracked for this ServiceReference object.
+     * If this method returns {@code true}, the service object becomes available from the {@link Tracker#getService()} and
+     * {@link Tracker#getServices()} methods. However notice that the service is still not accessible at that time.
      * @param reference the Reference to service being added to the Tracker object.
-     * @return The service object to be tracked for the ServiceReference object or null if the ServiceReference object should not be tracked.
+     * @return {@code true} if the service reference must be tracked. {@code false} ff the service reference must be
+     * ignored (un-tracked)
      */
     boolean addingService(ServiceReference reference);
     
     /**
      * A service tracked by the Tracker object has been added in the list.
-     * This method is called when a service has been added in the managed list (after addingService) and if the service has not disappeared before during the callback.
+     * This method is called when a service has been added in the managed list (after addingService) and if the
+     * service has not disappeared before during the callback.
+     * In this method, the service object is accessible from the {@link Tracker#getService()} and
+     * {@link Tracker#getServices()} method.
      * @param reference the added reference.
      */
     void addedService(ServiceReference reference);
 
     /**
      * A service tracked by the Tracker object has been modified.
-     * This method is called when a service being tracked by the Tracker object has had it properties modified.
-     * @param reference the Reference to service that has been modified.
+     * This method is called when a service tracked by the Tracker has its properties modified.
+     * @param reference the reference to service that has been modified.
      * @param service The service object for the modified service.
      */
     void modifiedService(ServiceReference reference, Object service);
 
     /**
      * A service tracked by the Tracker object has been removed.
-     * This method is called after a service is no longer being tracked by the Tracker object.
-     * @param reference the Reference to service that has been removed.
+     * This method is called when a tracked service is no longer being tracked by the Tracker object.
+     * Notice that some OSGi implementations does not support getting the service object from the bundle context in
+     * this method. The cached (given) object must be used.
+     * @param reference the reference to service that has been removed.
      * @param service The service object for the removed service.
      */
     void removedService(ServiceReference reference, Object service);
