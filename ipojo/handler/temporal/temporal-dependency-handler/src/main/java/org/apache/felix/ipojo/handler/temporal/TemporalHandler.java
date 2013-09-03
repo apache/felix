55c8823e3ab1cf08e9b6674a3f407ed7b1196b7d
Clement Escoffier <clement@apache.org>
2013-09-03 09:19:52 +0000

    Migrate temporal dependency handler tests to new IT structure
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1519609 13f79535-47bb-0310-9956-ffa450edef68

--- ipojo/handler/temporal/temporal-dependency-handler/src/main/java/org/apache/felix/ipojo/handler/temporal/TemporalHandler.java
+++ ipojo/handler/temporal/temporal-dependency-handler/src/main/java/org/apache/felix/ipojo/handler/temporal/TemporalHandler.java
@@ -18,37 +18,33 @@
  */
 package org.apache.felix.ipojo.handler.temporal;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Dictionary;
-import java.util.Hashtable;
-import java.util.List;
-
 import org.apache.felix.ipojo.ConfigurationException;
 import org.apache.felix.ipojo.PrimitiveHandler;
 import org.apache.felix.ipojo.metadata.Element;
 import org.apache.felix.ipojo.parser.FieldMetadata;
 import org.apache.felix.ipojo.parser.MethodMetadata;
 import org.apache.felix.ipojo.parser.PojoMetadata;
+import org.apache.felix.ipojo.util.DependencyMetadataHelper;
 import org.apache.felix.ipojo.util.DependencyModel;
 import org.apache.felix.ipojo.util.DependencyStateListener;
-import org.apache.felix.ipojo.util.DependencyMetadataHelper;
 import org.osgi.framework.Filter;
 import org.osgi.framework.InvalidSyntaxException;
 
+import java.util.*;
+
 /**
-* Temporal dependency handler.
-* A temporal dependency waits (block) for the availability of the service.
-* If no provider arrives in the specified among of time, a runtime exception is thrown.
-* @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
-*/
+ * Temporal dependency handler.
+ * A temporal dependency waits (block) for the availability of the service.
+ * If no provider arrives in the specified among of time, a runtime exception is thrown.
+ *
+ * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
+ */
 public class TemporalHandler extends PrimitiveHandler implements DependencyStateListener {
 
     /**
      * Default timeout if not specified.
      */
     public static final int DEFAULT_TIMEOUT = 3000;
-
     /**
      * No policy.
      */
@@ -69,12 +65,10 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
      * Uses {@code null}.
      */
     public static final int NULL = 4;
-
     /**
      * The handler namespace.
      */
     public static final String NAMESPACE = "org.apache.felix.ipojo.handler.temporal";
-
     /**
      * The list of managed dependencies.
      */
@@ -82,6 +76,7 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
 
     /**
      * Start method. Starts managed dependencies.
+     *
      * @see org.apache.felix.ipojo.Handler#start()
      */
     public void start() {
@@ -92,6 +87,7 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
 
     /**
      * Stop method. Stops managed dependencies.
+     *
      * @see org.apache.felix.ipojo.Handler#stop()
      */
     public void stop() {
@@ -103,7 +99,8 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
 
     /**
      * Configure method. Creates managed dependencies.
-     * @param meta the component type metadata.
+     *
+     * @param meta       the component type metadata.
      * @param dictionary the instance configuration.
      * @throws ConfigurationException if the dependency is not configured correctly
      * @see org.apache.felix.ipojo.Handler#configure(org.apache.felix.ipojo.metadata.Element, java.util.Dictionary)
@@ -114,20 +111,20 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
 
         // Also check with temporal is no requires.
         if (deps == null || deps.length == 0) {
-        	deps = meta.getElements("temporal", NAMESPACE);
+            deps = meta.getElements("temporal", NAMESPACE);
         }
-        
+
         // Get instance filters.
         Dictionary filtersConfiguration = getRequiresFilters(dictionary.get("temporal.filters"));
-        if(filtersConfiguration == null || filtersConfiguration.isEmpty()) {
-        	// Fall back on the Requires handler configuration, if any
-        	filtersConfiguration = getRequiresFilters(dictionary.get("requires.filters"));
+        if (filtersConfiguration == null || filtersConfiguration.isEmpty()) {
+            // Fall back on the Requires handler configuration, if any
+            filtersConfiguration = getRequiresFilters(dictionary.get("requires.filters"));
         }
         // Get from filters if any.
         Dictionary fromConfiguration = getRequiresFilters(dictionary.get("temporal.from"));
-        if(fromConfiguration == null || fromConfiguration.isEmpty()) {
-        	// Fall back on the Requires handler configuration, if any
-        	fromConfiguration = getRequiresFilters(dictionary.get("requires.from"));
+        if (fromConfiguration == null || fromConfiguration.isEmpty()) {
+            // Fall back on the Requires handler configuration, if any
+            fromConfiguration = getRequiresFilters(dictionary.get("requires.from"));
         }
 
 
@@ -202,16 +199,16 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
 
             String prox = deps[i].getAttribute("proxy");
             // Use proxy by default except for array:
-            boolean proxy = prox == null  || prox.equals("true");
+            boolean proxy = prox == null || prox.equals("true");
 
-            if (prox == null  && proxy) { // Proxy set because of the default.
-                if (agg  && ! collection) { // Aggregate and array
+            if (prox == null && proxy) { // Proxy set because of the default.
+                if (agg && !collection) { // Aggregate and array
                     proxy = false;
                 }
             }
 
             if (proxy && agg) {
-                if (! collection) {
+                if (!collection) {
                     error("Proxied aggregate temporal dependencies cannot be an array. Only collections are supported");
                 }
             }
@@ -219,7 +216,7 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
             long timeout = DEFAULT_TIMEOUT;
             if (deps[i].containsAttribute("timeout")) {
                 String to = deps[i].getAttribute("timeout");
-                if (to.equalsIgnoreCase("infinite")  || to.equalsIgnoreCase("-1")) {
+                if (to.equalsIgnoreCase("infinite") || to.equalsIgnoreCase("-1")) {
                     timeout = Long.MAX_VALUE; // Infinite wait time ...
                 } else {
                     timeout = new Long(deps[i].getAttribute("timeout")).longValue();
@@ -232,9 +229,9 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
             if (onTimeout != null) {
                 if (onTimeout.equalsIgnoreCase("nullable")) {
                     policy = NULLABLE;
-                } else if (onTimeout.equalsIgnoreCase("empty-array")  || onTimeout.equalsIgnoreCase("empty")) {
+                } else if (onTimeout.equalsIgnoreCase("empty-array") || onTimeout.equalsIgnoreCase("empty")) {
                     policy = EMPTY;
-                    if (! agg) {
+                    if (!agg) {
                         // The empty array policy can only be used on aggregate dependencies
                         error("Cannot use the empty array policy for " + field + " : non aggregate dependency.");
                     }
@@ -250,7 +247,7 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
             TemporalDependency dep = new TemporalDependency(specification, agg, collection, proxy, filter, getInstanceManager().getContext(), timeout, policy, di, this);
             m_dependencies.add(dep);
 
-            if (! proxy) { // Register method interceptor only if are not a proxy
+            if (!proxy) { // Register method interceptor only if are not a proxy
                 MethodMetadata[] methods = manipulation.getMethods();
                 for (int k = 0; k < methods.length; k++) {
                     getInstanceManager().register(methods[k], dep);
@@ -260,57 +257,61 @@ public class TemporalHandler extends PrimitiveHandler implements DependencyState
             getInstanceManager().register(fieldmeta, dep);
         }
     }
-    
-    /**
-	 * Gets the requires filter configuration from the given object.
-	 * The given object must come from the instance configuration.
-	 * This method was made to fix FELIX-2688. It supports filter configuration using
-	 * an array:
-	 * <code>{"myFirstDep", "(property1=value1)", "mySecondDep", "(property2=value2)"});</code>
-	 * 
-	 * Copied from DependencyHandler#getRequiresFilters(Object)
-	 * 
-	 * @param requiresFiltersValue the value contained in the instance
-	 * configuration.
-	 * @return the dictionary. If the object in already a dictionary, just returns it,
-	 * if it's an array, builds the dictionary.
-	 * @throws ConfigurationException the dictionary cannot be built
-	 */
-	private Dictionary getRequiresFilters(Object requiresFiltersValue)
-			throws ConfigurationException {
-		if (requiresFiltersValue != null
-				&& requiresFiltersValue.getClass().isArray()) {
-			String[] filtersArray = (String[]) requiresFiltersValue;
-			if (filtersArray.length % 2 != 0) {
-				throw new ConfigurationException(
-						"A requirement filter is invalid : "
-								+ requiresFiltersValue);
-			}
-			Dictionary requiresFilters = new Hashtable();
-			for (int i = 0; i < filtersArray.length; i += 2) {
-				requiresFilters.put(filtersArray[i], filtersArray[i + 1]);
-			}
-			return requiresFilters;
-		}
 
-		return (Dictionary) requiresFiltersValue;
-	}
+    /**
+     * Gets the requires filter configuration from the given object.
+     * The given object must come from the instance configuration.
+     * This method was made to fix FELIX-2688. It supports filter configuration using
+     * an array:
+     * <code>{"myFirstDep", "(property1=value1)", "mySecondDep", "(property2=value2)"});</code>
+     * <p/>
+     * Copied from DependencyHandler#getRequiresFilters(Object)
+     *
+     * @param requiresFiltersValue the value contained in the instance
+     *                             configuration.
+     * @return the dictionary. If the object in already a dictionary, just returns it,
+     *         if it's an array, builds the dictionary.
+     * @throws ConfigurationException the dictionary cannot be built
+     */
+    private Dictionary getRequiresFilters(Object requiresFiltersValue)
+            throws ConfigurationException {
+        if (requiresFiltersValue != null
+                && requiresFiltersValue.getClass().isArray()) {
+            String[] filtersArray = (String[]) requiresFiltersValue;
+            if (filtersArray.length % 2 != 0) {
+                throw new ConfigurationException(
+                        "A requirement filter is invalid : "
+                                + requiresFiltersValue);
+            }
+            Dictionary requiresFilters = new Hashtable();
+            for (int i = 0; i < filtersArray.length; i += 2) {
+                requiresFilters.put(filtersArray[i], filtersArray[i + 1]);
+            }
+            return requiresFilters;
+        }
+
+        return (Dictionary) requiresFiltersValue;
+    }
 
     /**
      * Nothing to do.
      * A temporal dependency is always valid.
+     *
      * @param dependencymodel dependency.
      * @see org.apache.felix.ipojo.util.DependencyStateListener#invalidate(org.apache.felix.ipojo.util.DependencyModel)
      */
-    public void invalidate(DependencyModel dependencymodel) {    }
+    public void invalidate(DependencyModel dependencymodel) {
+    }
 
     /**
      * Nothing to do.
      * A temporal dependency is always valid.
+     *
      * @param dependencymodel dependency.
      * @see org.apache.felix.ipojo.util.DependencyStateListener#validate(org.apache.felix.ipojo.util.DependencyModel)
      */
-    public void validate(DependencyModel dependencymodel) {    }
+    public void validate(DependencyModel dependencymodel) {
+    }
 
 
 }
