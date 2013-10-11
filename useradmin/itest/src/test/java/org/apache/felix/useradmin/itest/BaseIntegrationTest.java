517325d2c8332318bc1956627edb1ec4ff0dca80
Jan Willem Janssen <jawi@apache.org>
2013-10-11 08:59:14 +0000

    FELIX-4279, FELIX-4280:
    - ensure the org.osgi.service.useradmin package is exported by our
      implementation as well;
    - ensure the correct import dependencies are used, without hard
      dependencies on other optional services (such as EventAdmin);
    - stripped the itests from all redundant bundles to verify this.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1531234 13f79535-47bb-0310-9956-ffa450edef68

--- useradmin/itest/src/test/java/org/apache/felix/useradmin/itest/BaseIntegrationTest.java
+++ useradmin/itest/src/test/java/org/apache/felix/useradmin/itest/BaseIntegrationTest.java
@@ -78,9 +78,6 @@ public abstract class BaseIntegrationTest {
             url("link:classpath:META-INF/links/org.ops4j.pax.swissbox.framework.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
             url("link:classpath:META-INF/links/org.apache.geronimo.specs.atinject.link").startLevel(START_LEVEL_SYSTEM_BUNDLES),
 
-//            mavenBundle("org.osgi", "org.osgi.core").version("4.2.0").startLevel(START_LEVEL_SYSTEM_BUNDLES),
-            mavenBundle("org.osgi", "org.osgi.compendium").version("4.2.0").startLevel(START_LEVEL_SYSTEM_BUNDLES),
-            mavenBundle("org.apache.felix", "org.apache.felix.log").version("1.0.1").startLevel(START_LEVEL_SYSTEM_BUNDLES),
             mavenBundle("org.apache.felix", ORG_APACHE_FELIX_USERADMIN).versionAsInProject().startLevel(START_LEVEL_SYSTEM_BUNDLES),
             mavenBundle("org.apache.felix", ORG_APACHE_FELIX_USERADMIN_FILESTORE).versionAsInProject().startLevel(START_LEVEL_SYSTEM_BUNDLES),
             
