18c5aa40f881d295bc4ef576fc7145d834390501
Pierre De Rop <pderop@apache.org>
2013-10-13 08:48:30 +0000

    import cleanup
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/dependencymanager@1531635 13f79535-47bb-0310-9956-ffa450edef68

--- test2/src/test/java/org/apache/felix/dependencymanager/test2/integration/api/AdapterWithAspectCallbacksTest.java
+++ test2/src/test/java/org/apache/felix/dependencymanager/test2/integration/api/AdapterWithAspectCallbacksTest.java
@@ -18,29 +18,20 @@
  */
 package org.apache.felix.dependencymanager.test2.integration.api;
 
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.ops4j.pax.exam.CoreOptions.provision;
-
 import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.List;
 
+import org.apache.felix.dependencymanager.test2.components.Ensure;
+import org.apache.felix.dependencymanager.test2.integration.common.TestBase;
 import org.apache.felix.dm.Component;
 import org.apache.felix.dm.DependencyManager;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.apache.felix.dependencymanager.test2.components.Ensure;
-import org.apache.felix.dependencymanager.test2.integration.common.TestBase;
+import org.osgi.framework.ServiceReference;
 
 @RunWith(PaxExam.class)
 public class AdapterWithAspectCallbacksTest extends TestBase {
