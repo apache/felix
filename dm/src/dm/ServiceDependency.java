abc49946d35e92af3026eb9c133c1995b43f6a8d
Pierre De Rop <pderop@apache.org>
2014-02-26 22:31:12 +0000

    Extends ComponentDependencyDeclaration. Added setDebug method.
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1572306 13f79535-47bb-0310-9956-ffa450edef68

--- dm/src/dm/ServiceDependency.java
+++ dm/src/dm/ServiceDependency.java
@@ -2,10 +2,11 @@ package dm;
 
 import org.osgi.framework.ServiceReference;
 
-public interface ServiceDependency extends Dependency {
+public interface ServiceDependency extends Dependency, ComponentDependencyDeclaration {
     public ServiceDependency setService(Class serviceName);
     public ServiceDependency setService(Class serviceName, String serviceFilter);
     public ServiceDependency setService(String serviceFilter);
     public ServiceDependency setService(Class serviceName, ServiceReference serviceReference);
     public ServiceDependency setDefaultImplementation(Object implementation);
+    public ServiceDependency setDebug(String identifier);
 }
