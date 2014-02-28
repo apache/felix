fef50bdce8f96fd982f83dd9f96864e34ea50a7a
Pierre De Rop <pderop@apache.org>
2014-02-28 10:16:15 +0000

    some cleanup
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1572902 13f79535-47bb-0310-9956-ffa450edef68

--- dm/src/dm/ServiceDependency.java
+++ dm/src/dm/ServiceDependency.java
@@ -3,10 +3,10 @@ package dm;
 import org.osgi.framework.ServiceReference;
 
 public interface ServiceDependency extends Dependency, ComponentDependencyDeclaration {
-    public ServiceDependency setService(Class serviceName);
-    public ServiceDependency setService(Class serviceName, String serviceFilter);
+    public ServiceDependency setService(Class<?> serviceName);
+    public ServiceDependency setService(Class<?> serviceName, String serviceFilter);
     public ServiceDependency setService(String serviceFilter);
-    public ServiceDependency setService(Class serviceName, ServiceReference serviceReference);
+    public ServiceDependency setService(Class<?> serviceName, ServiceReference serviceReference);
     public ServiceDependency setDefaultImplementation(Object implementation);
     public ServiceDependency setPropagate(boolean propagate);
     public ServiceDependency setPropagate(Object instance, String method);
