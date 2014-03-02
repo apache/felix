27f436a7a2b24af4b4f5b7db1d0fb7d71c03e4e2
Pierre De Rop <pderop@apache.org>
2014-03-02 22:40:25 +0000

    added setCallbacks/setAutoConfig methods
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1573383 13f79535-47bb-0310-9956-ffa450edef68

--- dm/src/dm/ServiceDependency.java
+++ dm/src/dm/ServiceDependency.java
@@ -3,6 +3,14 @@ package dm;
 import org.osgi.framework.ServiceReference;
 
 public interface ServiceDependency extends Dependency, ComponentDependencyDeclaration {
+	public ServiceDependency setCallbacks(String add, String remove);
+	public ServiceDependency setCallbacks(String add, String change, String remove);
+	public ServiceDependency setCallbacks(Object instance, String add, String remove);
+	public ServiceDependency setCallbacks(Object instance, String add, String change, String remove);
+	public ServiceDependency setRequired(boolean required);
+	public ServiceDependency setAutoConfig(boolean autoConfig);
+    public ServiceDependency setAutoConfig(String instanceName);
+
     public ServiceDependency setService(Class<?> serviceName);
     public ServiceDependency setService(Class<?> serviceName, String serviceFilter);
     public ServiceDependency setService(String serviceFilter);
@@ -10,5 +18,4 @@ public interface ServiceDependency extends Dependency, ComponentDependencyDeclar
     public ServiceDependency setDefaultImplementation(Object implementation);
     public ServiceDependency setPropagate(boolean propagate);
     public ServiceDependency setPropagate(Object instance, String method);
-    public ServiceDependency setDebug(String identifier);
 }
