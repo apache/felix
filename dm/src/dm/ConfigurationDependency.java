8cb1ae6300f0b06772d7bdc79ca02bd86cd1dddd
Pierre De Rop <pderop@apache.org>
2014-03-02 22:37:22 +0000

    added ConfigurationDependency interface
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1573379 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dm/src/dm/ConfigurationDependency.java
@@ -0,0 +1,11 @@
+package dm;
+
+public interface ConfigurationDependency extends Dependency, ComponentDependencyDeclaration {
+	ConfigurationDependency setCallback(String callback);
+	ConfigurationDependency setPid(String pid);
+	ConfigurationDependency setPropagate(boolean propagate);
+	ConfigurationDependency setHeading(String heading);
+	ConfigurationDependency setDescription(String description);
+	ConfigurationDependency setLocalization(String path);
+	ConfigurationDependency add(PropertyMetaData properties);
+}
