9fcc7ea0df6c538ded38172ef60f73a25d9c8643
Pierre De Rop <pderop@apache.org>
2014-03-05 15:38:03 +0000

    Introduced Activator in order to register ComponentAdmin service
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1574532 13f79535-47bb-0310-9956-ffa450edef68

---
+++ dm/src/dm/impl/Activator.java
@@ -0,0 +1,27 @@
+package dm.impl;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import dm.admin.ComponentAdmin;
+
+/**
+ * DependencyManager activator, used to register the DM Admin Service.
+ */
+public class Activator implements BundleActivator {
+    // the service registration of the DependencyManager Admin Service.
+    private volatile ServiceRegistration m_adminReg;
+
+    public void start(BundleContext context) throws Exception {
+        context.registerService(ComponentAdmin.class.getName(), new ComponentAdminImpl(), null);
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        final ServiceRegistration adminReg = m_adminReg;
+        m_adminReg = null;
+        if (adminReg != null) {
+            adminReg.unregister();
+        }
+    }
+}
