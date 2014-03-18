6af9f608f02c454292a9b79a57f330ebb3a7d699
Pierre De Rop <pderop@apache.org>
2014-03-18 21:27:49 +0000

    Added BundleDependency, ResourceDependency, TemporalServiceDependency, FactoryConfigurationAdapter, BundleAdapter, ResourceAdapter.
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1579045 13f79535-47bb-0310-9956-ffa450edef68

--- dm/src/dm/DependencyManager.java
+++ dm/src/dm/DependencyManager.java
@@ -20,7 +20,6 @@ package dm;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -33,11 +32,17 @@ import org.osgi.framework.BundleException;
 import org.osgi.framework.FrameworkUtil;
 
 import dm.context.ComponentContext;
+import dm.impl.FactoryConfigurationAdapterImpl;
 import dm.impl.AdapterServiceImpl;
+import dm.impl.BundleAdapterImpl;
+import dm.impl.BundleDependencyImpl;
 import dm.impl.ComponentImpl;
 import dm.impl.ConfigurationDependencyImpl;
 import dm.impl.Logger;
+import dm.impl.ResourceAdapterImpl;
+import dm.impl.ResourceDependencyImpl;
 import dm.impl.ServiceDependencyImpl;
+import dm.impl.TemporalServiceDependencyImpl;
 import dm.impl.index.AdapterFilterIndex;
 import dm.impl.index.AspectFilterIndex;
 import dm.impl.index.ServiceRegistryCache;
@@ -157,6 +162,18 @@ public class DependencyManager {
         return new ConfigurationDependencyImpl(m_context, m_logger);
     }
     
+    public BundleDependency createBundleDependency() {
+        return new BundleDependencyImpl(m_context, m_logger);
+    }
+
+    public ResourceDependency createResourceDependency() {
+        return new ResourceDependencyImpl(m_context, m_logger);
+    }
+
+    public TemporalServiceDependency createTemporalServiceDependency() {
+        return new TemporalServiceDependencyImpl(m_context, m_logger);
+    }
+
     public Component createAdapterService(Class serviceInterface, String serviceFilter, String autoConfig) {
         return new AdapterServiceImpl(this, serviceInterface, serviceFilter, autoConfig, null, null, null);
     }
@@ -173,7 +190,35 @@ public class DependencyManager {
         return new AdapterServiceImpl(this, serviceInterface, serviceFilter, null, add, change, remove);
     }
 
-    public List<Component> getComponents() {
+    public Component createAdapterFactoryConfigurationService(String factoryPid, String update, boolean propagate) {
+        return new FactoryConfigurationAdapterImpl(this, factoryPid, update, propagate);
+    }
+
+    public Component createAdapterFactoryConfigurationService(String factoryPid, String update, boolean propagate, String heading, String desc, String localization, PropertyMetaData[] propertiesMetaData) {
+        return new FactoryConfigurationAdapterImpl(this, factoryPid, update, propagate, m_context, m_logger, heading, desc, localization, propertiesMetaData);
+    }
+    
+    public Component createBundleAdapterService(int bundleStateMask, String bundleFilter, boolean propagate) {
+        return new BundleAdapterImpl(this, bundleStateMask, bundleFilter, propagate);
+    }
+
+    public Component createResourceAdapterService(String resourceFilter, boolean propagate, Object callbackInstance, String callbackChanged) {
+        return new ResourceAdapterImpl(this, resourceFilter, propagate, callbackInstance, null, callbackChanged);
+    }
+    
+    public Component createResourceAdapterService(String resourceFilter, boolean propagate, Object callbackInstance, String callbackSet, String callbackChanged) {
+        return new ResourceAdapterImpl(this, resourceFilter, propagate, callbackInstance, callbackSet, callbackChanged);
+    }
+    
+    public Component createResourceAdapterService(String resourceFilter, Object propagateCallbackInstance, String propagateCallbackMethod, Object callbackInstance, String callbackChanged) {
+        return new ResourceAdapterImpl(this, resourceFilter, propagateCallbackInstance, propagateCallbackMethod, callbackInstance, null, callbackChanged);
+    }
+    
+    public Component createResourceAdapterService(String resourceFilter, Object propagateCallbackInstance, String propagateCallbackMethod, Object callbackInstance, String callbackSet, String callbackChanged) {
+        return new ResourceAdapterImpl(this, resourceFilter, propagateCallbackInstance, propagateCallbackMethod, callbackInstance, callbackSet, callbackChanged);
+    }
+    
+   public List<Component> getComponents() {
         return m_components;
     }
 
