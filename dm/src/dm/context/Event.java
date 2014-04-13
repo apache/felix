2001a6535e12d127a5dd7f4300277c1f4cb82f6a
Pierre De Rop <pderop@apache.org>
2014-04-13 17:05:39 +0000

    Event extends the Comparable interface, because all kind of events must be comparable.
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1587044 13f79535-47bb-0310-9956-ffa450edef68

--- dm/src/dm/context/Event.java
+++ dm/src/dm/context/Event.java
@@ -6,7 +6,7 @@ import org.osgi.framework.BundleContext;
  * An event holds all data that belongs to some external event as it comes in via
  * the 'changed' callback of a dependency.
  */
-public interface Event {
+public interface Event extends Comparable {
     /**
      * Release the resources this event is holding (like service reference for example).
      * @param m_context the bundle context possibly used to clean some service references.s
