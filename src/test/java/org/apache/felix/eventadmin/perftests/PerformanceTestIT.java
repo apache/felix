e8da49e0f6c2b0c73a582a95384413b1733139a7
Carsten Ziegeler <cziegeler@apache.org>
2014-09-08 17:12:58 +0000

    FELIX-4630: Adding PerformanceTestIT to measure difference between send and post events
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/eventadmin/impl@1623433 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/felix/eventadmin/perftests/PerformanceTestIT.java
+++ src/test/java/org/apache/felix/eventadmin/perftests/PerformanceTestIT.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.felix.eventadmin.ittests;
+package org.apache.felix.eventadmin.perftests;
 
 import org.junit.After;
 import org.junit.Test;
