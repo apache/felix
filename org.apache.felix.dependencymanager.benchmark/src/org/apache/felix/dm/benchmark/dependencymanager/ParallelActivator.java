4033600cec1f875ee1cc6d255ceb15ab9889de60
Pierre De Rop <pderop@apache.org>
2014-09-22 21:59:46 +0000

    Register the threadpool in the registry. The DependencyManager.setThreadPool is not used anymore
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1626909 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.dependencymanager.benchmark/src/org/apache/felix/dm/benchmark/dependencymanager/ParallelActivator.java
+++ org.apache.felix.dependencymanager.benchmark/src/org/apache/felix/dm/benchmark/dependencymanager/ParallelActivator.java
@@ -1,5 +1,8 @@
 package org.apache.felix.dm.benchmark.dependencymanager;
 
+import java.util.Hashtable;
+import java.util.concurrent.Executor;
+
 import org.apache.felix.dm.DependencyManager;
 import org.apache.felix.dm.benchmark.scenario.Helper;
 import org.osgi.framework.BundleContext;
@@ -8,8 +11,10 @@ import org.osgi.framework.BundleContext;
  * Parallel version of our default Activator.
  */
 public class ParallelActivator extends Activator {
-    public void init(BundleContext context, DependencyManager mgr) throws Exception {  
-        mgr.setThreadPool(Helper.getThreadPool());
+    public void init(BundleContext context, DependencyManager mgr) throws Exception {
+        Hashtable<String, String> props = new Hashtable<>();
+        props.put("target", DependencyManager.THREADPOOL);
+        context.registerService(Executor.class.getName(), Helper.getThreadPool(), props);
         super.init(context, mgr);
     }
 }
