3056e8604e883c542d4605216dfed6e3da892cb8
Pierre De Rop <pderop@apache.org>
2014-10-30 20:51:03 +0000

    Use a LogService when an added file is detected
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1635611 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.dependencymanager.samples/src/org/apache/felix/dependencymanager/samples/customdep/Activator.java
+++ org.apache.felix.dependencymanager.samples/src/org/apache/felix/dependencymanager/samples/customdep/Activator.java
@@ -3,12 +3,14 @@ package org.apache.felix.dependencymanager.samples.customdep;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.osgi.framework.BundleContext;
+import org.osgi.service.log.LogService;
 
 public class Activator extends DependencyActivatorBase {
     @Override
     public void init(BundleContext context, DependencyManager m) throws Exception {
         m.add(createComponent()
             .setImplementation(PathTracker.class)
+            .add(createServiceDependency().setService(LogService.class).setRequired(true))
             .add(new PathDependency("/tmp").setCallbacks("add", "change", "remove")));
     }
 }
--- org.apache.felix.dependencymanager.samples/src/org/apache/felix/dependencymanager/samples/customdep/PathTracker.java
+++ org.apache.felix.dependencymanager.samples/src/org/apache/felix/dependencymanager/samples/customdep/PathTracker.java
@@ -1,7 +1,11 @@
 package org.apache.felix.dependencymanager.samples.customdep;
 
+import org.osgi.service.log.LogService;
+
 public class PathTracker {
+    volatile LogService logService;
+    
     void add(String path) {
-        System.out.println("PathTracker.add: " + path);
+        logService.log(LogService.LOG_INFO, "PathTracker.add: " + path);
     }
 }
