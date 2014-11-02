a02c8d5a9308603754da5d377297c9f5cb43bf05
Pierre De Rop <pderop@apache.org>
2014-11-02 20:45:37 +0000

    added more tests
    git-svn-id: https://svn.apache.org/repos/asf/felix/sandbox/pderop/dependencymanager-prototype@1636187 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.dependencymanager.itest/src/org/apache/felix/dm/itest/FELIX4158_DependencyDeclarationTest.java
+++ org.apache.felix.dependencymanager.itest/src/org/apache/felix/dm/itest/FELIX4158_DependencyDeclarationTest.java
@@ -44,22 +44,6 @@ public class FELIX4158_DependencyDeclarationTest extends TestBase {
         Assert.assertNull(cdds[0].getFilter());
     }
     
-    public void testResourceWithFilterDependencyDeclaration() {
-        DependencyManager m = getDM();
-        Component c = m.createComponent()
-            .setImplementation(new Object())
-            .add(m.createResourceDependency()
-                .setFilter("(&(path=/path/to/*.txt)(host=localhost))"));
-        
-        ComponentDeclaration cd = c.getComponentDeclaration();
-        ComponentDependencyDeclaration[] cdds = cd.getComponentDependencies();
-        Assert.assertNotNull(cdds);
-        Assert.assertNotNull(cdds.length == 1);
-        Assert.assertEquals(cdds[0].getName(), ("(&(path=/path/to/*.txt)(host=localhost))"));
-        Assert.assertNotNull(cdds[0].getFilter());
-        Assert.assertEquals(cdds[0].getFilter(), "(&(path=/path/to/*.txt)(host=localhost))");
-    }
-    
     public void testResourceDependencyDeclaration() throws MalformedURLException {
         DependencyManager m = getDM();
         Component c = m.createComponent()
@@ -72,10 +56,62 @@ public class FELIX4158_DependencyDeclarationTest extends TestBase {
         Assert.assertNotNull(cdds);
         Assert.assertNotNull(cdds.length == 1);
         Assert.assertEquals(cdds[0].getName(), "file://localhost/path/to/file1.txt");
+        Assert.assertNotNull(cdds[0].getSimpleName());
+        Assert.assertEquals(cdds[0].getSimpleName(), "file://localhost/path/to/file1.txt");
         Assert.assertNull(cdds[0].getFilter());
     }
     
+    public void testResourceDependencyDeclarationWithFilter() {
+        DependencyManager m = getDM();
+        Component c = m.createComponent()
+            .setImplementation(new Object())
+            .add(m.createResourceDependency()
+                .setFilter("(&(path=/path/to/*.txt)(host=localhost))"));
+        
+        ComponentDeclaration cd = c.getComponentDeclaration();
+        ComponentDependencyDeclaration[] cdds = cd.getComponentDependencies();
+        Assert.assertNotNull(cdds);
+        Assert.assertNotNull(cdds.length == 1);
+        Assert.assertEquals(cdds[0].getName(), ("(&(path=/path/to/*.txt)(host=localhost))"));
+        Assert.assertNull(cdds[0].getSimpleName());
+        Assert.assertNotNull(cdds[0].getFilter());
+        Assert.assertEquals(cdds[0].getFilter(), "(&(path=/path/to/*.txt)(host=localhost))");
+    }
+    
     public void testBundleDependencyDeclaration() throws MalformedURLException {
+        DependencyManager m = getDM();
+        Component c = m.createComponent()
+            .setImplementation(new Object())
+            .add(m.createBundleDependency());
+        
+        ComponentDeclaration cd = c.getComponentDeclaration();
+        ComponentDependencyDeclaration[] cdds = cd.getComponentDependencies();
+        Assert.assertNotNull(cdds);
+        Assert.assertNotNull(cdds.length == 1);
+        Assert.assertEquals(cdds[0].getName(), "active installed resolved");
+        Assert.assertNotNull(cdds[0].getSimpleName());
+        Assert.assertEquals(cdds[0].getSimpleName(), "active installed resolved");
+        Assert.assertNull(cdds[0].getFilter());
+    }
+
+    public void testBundleDependencyDeclarationWithMask() throws MalformedURLException {
+        DependencyManager m = getDM();
+        Component c = m.createComponent()
+            .setImplementation(new Object())
+            .add(m.createBundleDependency()
+                .setStateMask( Bundle.ACTIVE | Bundle.RESOLVED));
+        
+        ComponentDeclaration cd = c.getComponentDeclaration();
+        ComponentDependencyDeclaration[] cdds = cd.getComponentDependencies();
+        Assert.assertNotNull(cdds);
+        Assert.assertNotNull(cdds.length == 1);
+        Assert.assertEquals(cdds[0].getName(), "active resolved");
+        Assert.assertNotNull(cdds[0].getSimpleName());
+        Assert.assertEquals(cdds[0].getSimpleName(), "active resolved");
+        Assert.assertNull(cdds[0].getFilter());
+    }
+    
+    public void testBundleDependencyDeclarationWithFilter() throws MalformedURLException {
         DependencyManager m = getDM();
         Component c = m.createComponent()
             .setImplementation(new Object())
@@ -88,6 +124,8 @@ public class FELIX4158_DependencyDeclarationTest extends TestBase {
         Assert.assertNotNull(cdds);
         Assert.assertNotNull(cdds.length == 1);
         Assert.assertEquals(cdds[0].getName(), "active (DependencyManager-Component=*)");
+        Assert.assertNotNull(cdds[0].getSimpleName());
+        Assert.assertEquals(cdds[0].getSimpleName(), "active");
         Assert.assertNotNull(cdds[0].getFilter());
         Assert.assertEquals(cdds[0].getFilter(), "(DependencyManager-Component=*)");
     }
