f03aa7109be552f5be155d513c0f58c791c735af
Carsten Ziegeler <cziegeler@apache.org>
2015-02-02 14:49:40 +0000

    FELIX-4060 : Implement HTTP Service Update (RFC-189)
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1656479 13f79535-47bb-0310-9956-ffa450edef68

--- http/base/src/main/java/org/apache/felix/http/base/internal/whiteboard/ContextHandler.java
+++ http/base/src/main/java/org/apache/felix/http/base/internal/whiteboard/ContextHandler.java
@@ -69,6 +69,7 @@ public final class ContextHandler implements Comparable<ContextHandler>
         this.sharedContext = new SharedServletContextImpl(webContext,
                 info.getPrefix(),
                 info.getName(),
+                info.getInitParams(),
                 new ServletContextAttributeListener() {
 
                     @Override
