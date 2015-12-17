00f127736a8f145d858697bfe54eccc0bfd0b0e6
Carsten Ziegeler <cziegeler@apache.org>
2015-12-17 10:51:34 +0000

    Rename filter example to whiteboard
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1720519 13f79535-47bb-0310-9956-ffa450edef68

--- http/samples/whiteboard/src/main/java/org/apache/felix/http/samples/whiteboard/Activator.java
+++ http/samples/whiteboard/src/main/java/org/apache/felix/http/samples/whiteboard/Activator.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.felix.http.samples.filter;
+package org.apache.felix.http.samples.whiteboard;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
--- http/samples/whiteboard/src/main/java/org/apache/felix/http/samples/whiteboard/TestServlet.java
+++ http/samples/whiteboard/src/main/java/org/apache/felix/http/samples/whiteboard/TestServlet.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.felix.http.samples.filter;
+package org.apache.felix.http.samples.whiteboard;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
