f4d738631ea54b3d3c8692adba42e008e9422d0c
Guillaume Nodet <gnodet@apache.org>
2016-03-21 16:53:06 +0000

    Add a JLine based shell
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1735995 13f79535-47bb-0310-9956-ffa450edef68

---
+++ gogo/jline/src/main/java/org/apache/felix/gogo/jline/Highlighter.java
@@ -0,0 +1,274 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.gogo.jline;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.felix.gogo.runtime.CommandSessionImpl;
+import org.apache.felix.gogo.runtime.EOFError;
+import org.apache.felix.gogo.runtime.Parser.Program;
+import org.apache.felix.gogo.runtime.Parser.Statement;
+import org.apache.felix.gogo.runtime.SyntaxError;
+import org.apache.felix.gogo.runtime.Token;
+import org.apache.felix.service.command.CommandSession;
+import org.apache.felix.service.command.Function;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReader.RegionType;
+import org.jline.reader.impl.DefaultHighlighter;
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStringBuilder;
+import org.jline.utils.AttributedStyle;
+import org.jline.utils.WCWidth;
+
+public class Highlighter extends DefaultHighlighter {
+
+    private final CommandSession session;
+
+    public Highlighter(CommandSession session) {
+        this.session = session;
+    }
+
+    public AttributedString highlight(LineReader reader, String buffer) {
+        try {
+            Program program = null;
+            List<Token> tokens = null;
+            List<Statement> statements = null;
+            String repaired = buffer + " ";
+            while (program == null) {
+                try {
+                    org.apache.felix.gogo.runtime.Parser parser = new org.apache.felix.gogo.runtime.Parser(repaired);
+                    program = parser.program();
+                    tokens = parser.tokens();
+                    statements = parser.statements();
+                } catch (EOFError e) {
+                    repaired = repaired + e.repair();
+                }
+            }
+
+            int underlineStart = -1;
+            int underlineEnd = -1;
+            int negativeStart = -1;
+            int negativeEnd = -1;
+            String search = reader.getSearchTerm();
+            if (search != null && search.length() > 0) {
+                underlineStart = buffer.indexOf(search);
+                if (underlineStart >= 0) {
+                    underlineEnd = underlineStart + search.length() - 1;
+                }
+            }
+            if (reader.getRegionActive() != RegionType.NONE) {
+                negativeStart = reader.getRegionMark();
+                negativeEnd = reader.getBuffer().cursor();
+                if (negativeStart > negativeEnd) {
+                    int x = negativeEnd;
+                    negativeEnd = negativeStart;
+                    negativeStart = x;
+                }
+                if (reader.getRegionActive() == RegionType.LINE) {
+                    while (negativeStart > 0 && reader.getBuffer().atChar(negativeStart - 1) != '\n') {
+                        negativeStart--;
+                    }
+                    while (negativeEnd < reader.getBuffer().length() - 1 && reader.getBuffer().atChar(negativeEnd + 1) != '\n') {
+                        negativeEnd++;
+                    }
+                }
+            }
+
+            Type[] types = new Type[repaired.length()];
+
+            Arrays.fill(types, Type.Unknown);
+
+            int cur = 0;
+            for (Token token : tokens) {
+                // We're on the repair side, so exit now
+                if (token.start() >= buffer.length()) {
+                    break;
+                }
+                if (token.start() > cur) {
+//                    ansi.a(buffer.substring(cur, token.start()));
+                    cur = token.start();
+                }
+                // Find corresponding statement
+                Statement statement = null;
+                for (int i = statements.size() - 1; i >= 0; i--) {
+                    Statement s = statements.get(i);
+                    if (s.start() <= cur && cur < s.start() + s.length()) {
+                        statement = s;
+                        break;
+                    }
+                }
+
+                // Reserved tokens
+                Type type = Type.Unknown;
+                if (Token.eq(token, "{")
+                        || Token.eq(token, "}")
+                        || Token.eq(token, "(")
+                        || Token.eq(token, ")")
+                        || Token.eq(token, "[")
+                        || Token.eq(token, "]")
+                        || Token.eq(token, "|")
+                        || Token.eq(token, ";")
+                        || Token.eq(token, "=")) {
+                    type = Type.Reserved;
+                } else if (token.charAt(0) == '\'' || token.charAt(0) == '"') {
+                    type = Type.String;
+                } else if (token.toString().matches("^[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?$")) {
+                    type = Type.Number;
+                } else if (token.charAt(0) == '$') {
+                    type = Type.Variable;
+                } else if (((Set) session.get(CommandSessionImpl.CONSTANTS)).contains(token.toString())
+                        || Token.eq(token, "null") || Token.eq(token, "false") || Token.eq(token, "true")) {
+                    type = Type.Constant;
+                } else {
+                    boolean isFirst = statement != null && statement.tokens().size() > 0
+                            && token == statement.tokens().get(0);
+                    boolean isThirdWithNext = statement != null && statement.tokens().size() > 3
+                            && token == statement.tokens().get(2);
+                    boolean isAssign = statement != null && statement.tokens().size() > 1
+                            && Token.eq(statement.tokens().get(1), "=");
+                    if (isFirst && isAssign) {
+                        type = Type.VariableName;
+                    }
+                    if (isFirst && !isAssign || isAssign && isThirdWithNext) {
+                        Object v = session.get(Shell.resolve(session, token.toString()));
+                        type = (v instanceof Function) ? Type.Function : Type.BadFunction;
+                    }
+                }
+                Arrays.fill(types, token.start(), Math.min(token.start() + token.length(), types.length), type);
+                /*
+                String valid;
+                if (token.start() + token.length() <= buffer.length()) {
+                    valid = token.toString();
+                } else {
+                    valid = token.subSequence(0, buffer.length() - token.start()).toString();
+                }
+                switch (type) {
+                    case Reserved:
+                        ansi.fg(Color.MAGENTA).a(valid).fg(Color.DEFAULT);
+                        break;
+                    case String:
+                    case Number:
+                    case Constant:
+                        ansi.fg(Color.GREEN).a(valid).fg(Color.DEFAULT);
+                        break;
+                    case Variable:
+                    case VariableName:
+                        ansi.fg(Color.CYAN).a(valid).fg(Color.DEFAULT);
+                        break;
+                    case Function:
+                        ansi.fg(Color.BLUE).a(valid).fg(Color.DEFAULT);
+                        break;
+                    case BadFunction:
+                        ansi.fg(Color.RED).a(valid).fg(Color.DEFAULT);
+                        break;
+                    default:
+                        ansi.a(valid);
+                        break;
+                }
+                */
+                cur = Math.min(token.start() + token.length(), buffer.length());
+            }
+
+            if (buffer.length() < repaired.length()) {
+                Arrays.fill(types, buffer.length(), repaired.length(), Type.Repair);
+            }
+
+            AttributedStringBuilder sb = new AttributedStringBuilder();
+            Type prevType = Type.Unknown;
+            for (int i = 0; i < repaired.length(); i++) {
+                if (i == underlineStart) {
+                    sb.style(sb.style().underline());
+                }
+                if (i == negativeStart) {
+                    sb.style(sb.style().inverse());
+                }
+                if (types[i] != prevType) {
+                    prevType = types[i];
+                    switch (prevType) {
+                        case Reserved:
+                            sb.style(sb.style().foreground(AttributedStyle.MAGENTA));
+                            break;
+                        case String:
+                        case Number:
+                        case Constant:
+                            sb.style(sb.style().foreground(AttributedStyle.GREEN));
+                            break;
+                        case Variable:
+                        case VariableName:
+                            sb.style(sb.style().foreground(AttributedStyle.CYAN));
+                            break;
+                        case Function:
+                            sb.style(sb.style().foreground(AttributedStyle.BLUE));
+                            break;
+                        case BadFunction:
+                            sb.style(sb.style().foreground(AttributedStyle.RED));
+                            break;
+                        case Repair:
+                            sb.style(sb.style().foreground(AttributedStyle.BLACK + AttributedStyle.BRIGHT));
+                            break;
+                        default:
+                            sb.style(sb.style().foregroundDefault());
+                            break;
+                    }
+                }
+                char c = repaired.charAt(i);
+                if (c == '\t' || c == '\n') {
+                    sb.append(c);
+                } else if (c < 32) {
+                    sb.style(sb.style().inverseNeg())
+                            .append('^')
+                            .append((char) (c + '@'))
+                            .style(sb.style().inverseNeg());
+                } else {
+                    int w = WCWidth.wcwidth(c);
+                    if (w > 0) {
+                        sb.append(c);
+                    }
+                }
+                if (i == underlineEnd) {
+                    sb.style(sb.style().underlineOff());
+                }
+                if (i == negativeEnd) {
+                    sb.style(sb.style().inverseOff());
+                }
+            }
+
+            return sb.toAttributedString();
+        } catch (SyntaxError e) {
+            return super.highlight(reader, buffer);
+        }
+    }
+
+    enum Type {
+        Reserved,
+        String,
+        Number,
+        Variable,
+        VariableName,
+        Function,
+        BadFunction,
+        Value,
+        Constant,
+        Unknown,
+        Repair
+    }
+
+}
---
+++ gogo/jline/src/main/java/org/apache/felix/gogo/jline/Procedural.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.gogo.jline;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.felix.service.command.CommandSession;
+import org.apache.felix.service.command.Function;
+
+public class Procedural {
+    static final String[] functions = {"each", "if", "not", "throw", "try", "until",
+            "while"};
+
+    public List<Object> each(CommandSession session, Collection<Object> list,
+                             Function closure) throws Exception {
+        List<Object> args = new ArrayList<Object>();
+        List<Object> results = new ArrayList<Object>();
+        args.add(null);
+
+        for (Object x : list) {
+            checkInterrupt();
+            args.set(0, x);
+            results.add(closure.execute(session, args));
+        }
+
+        return results;
+    }
+
+    public Object _if(CommandSession session, Function[] fns) throws Exception {
+        int length = fns.length;
+        if (length < 2) {
+            throw new IllegalArgumentException(
+                    "Usage: if {condition} {if-action} ... {else-action}");
+        }
+
+        for (int i = 0; i < length; ++i) {
+            if (i == length - 1 || isTrue(fns[i++].execute(session, null))) {
+                return fns[i].execute(session, null);
+            }
+        }
+
+        return null;
+    }
+
+    public boolean not(CommandSession session, Function condition) throws Exception {
+        if (null == condition) {
+            return true;
+        }
+
+        return !isTrue(condition.execute(session, null));
+    }
+
+    // Reflective.coerce() prefers to construct a new Throwable(String)
+    // than to call this method directly.
+    public void _throw(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    public void _throw(Exception e) throws Exception {
+        throw e;
+    }
+
+    public void _throw(CommandSession session) throws Throwable {
+        Object exception = session.get("exception");
+        if (exception instanceof Throwable)
+            throw (Throwable) exception;
+        else
+            throw new IllegalArgumentException("exception not set or not Throwable.");
+    }
+
+    public Object _try(CommandSession session, Function func) throws Exception {
+        try {
+            return func.execute(session, null);
+        } catch (Exception e) {
+            session.put("exception", e);
+            return null;
+        }
+    }
+
+    public Object _try(CommandSession session, Function func, Function error)
+            throws Exception {
+        try {
+            return func.execute(session, null);
+        } catch (Exception e) {
+            session.put("exception", e);
+            return error.execute(session, null);
+        }
+    }
+
+    public void _while(CommandSession session, Function condition, Function ifTrue)
+            throws Exception {
+        while (isTrue(condition.execute(session, null))) {
+            ifTrue.execute(session, null);
+        }
+    }
+
+    public void until(CommandSession session, Function condition, Function ifTrue)
+            throws Exception {
+        while (!isTrue(condition.execute(session, null))) {
+            ifTrue.execute(session, null);
+        }
+    }
+
+    private boolean isTrue(Object result) throws InterruptedException {
+        checkInterrupt();
+
+        if (result == null)
+            return false;
+
+        if (result instanceof Boolean)
+            return ((Boolean) result).booleanValue();
+
+        if (result instanceof Number) {
+            if (0 == ((Number) result).intValue())
+                return false;
+        }
+
+        if ("".equals(result))
+            return false;
+
+        if ("0".equals(result))
+            return false;
+
+        return true;
+    }
+
+    private void checkInterrupt() throws InterruptedException {
+        if (Thread.currentThread().isInterrupted())
+            throw new InterruptedException("loop interrupted");
+    }
+}
