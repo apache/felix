e3c69e1e0f50dca575a387ff9d2d80052758db89
Carsten Ziegeler <cziegeler@apache.org>
2016-04-27 00:11:39 +0000

    Update SCR example
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1741126 13f79535-47bb-0310-9956-ffa450edef68

--- examples/spellcheckscr/src/main/java/org/apache/felix/examples/spellcheckscr/SpellCheckServiceImpl.java
+++ examples/spellcheckscr/src/main/java/org/apache/felix/examples/spellcheckscr/SpellCheckServiceImpl.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2006 The Apache Software Foundation
+ *   Copyright 2006-2016 The Apache Software Foundation
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -18,71 +18,49 @@ package org.apache.felix.examples.spellcheckscr;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.felix.examples.dictionaryservice.DictionaryService;
 import org.apache.felix.examples.spellcheckservice.SpellCheckService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 
 /**
- * This class re-implements the spell check service of Example 5. This service
- * implementation behaves exactly like the one in Example 5, specifically, it
+ * This class re-implements the spell check service of Example 6. This service
+ * implementation behaves exactly like the one in Example 6, specifically, it
  * aggregates all available dictionary services, monitors their dynamic
  * availability, and only offers the spell check service if there are dictionary
  * services available. The service implementation is greatly simplified, though,
  * by using the Service Component Runtime. Notice that there is no OSGi references in the
- * application code; intead, the metadata.xml file describes the service
+ * application code; instead, the annotations describe the service
  * dependencies to the Service Component Runtime, which automatically manages them and it
  * also automatically registers the spell check services as appropriate.
- * 
+ *
  * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
  */
+@Component
 public class SpellCheckServiceImpl implements SpellCheckService
 {
-    // List of service objects.
-    private ArrayList m_svcObjList = new ArrayList();
-
-
     /**
-     * This method is used by the Service Component Runtime to add new dictionaries to the
-     * spell check service.
-     * 
-     * @param dictionary
-     *            the dictionary to add to the spell check service.
+     * List of service objects.
+     *
+     * This field is managed by the Service Component Runtime and updated
+     * with the current set of available dictionary services.
+     * At least one dictionary service is required.
      */
-    public void addDictionary( DictionaryService dictionary )
-    {
-        // Lock list and add service object.
-        synchronized ( m_svcObjList )
-        {
-            m_svcObjList.add( dictionary );
-        }
-    }
-
-
-    /**
-     * This method is used by the Service Component Runtime to remove dictionaries from the
-     * spell check service.
-     * 
-     * @param dictionary
-     *            the dictionary to remove from the spell check service.
-     */
-    public void removeDictionary( DictionaryService dictionary )
-    {
-        // Lock list and remove service object.
-        synchronized ( m_svcObjList )
-        {
-            m_svcObjList.remove( dictionary );
-        }
-    }
-
+    @Reference(policy=ReferencePolicy.DYNAMIC, cardinality=ReferenceCardinality.AT_LEAST_ONE)
+    private volatile List<DictionaryService> m_svcObjList;
 
     /**
      * Checks a given passage for spelling errors. A passage is any number of
      * words separated by a space and any of the following punctuation marks:
      * comma (,), period (.), exclamation mark (!), question mark (?),
      * semi-colon (;), and colon(:).
-     * 
+     *
      * @param passage
      *            the passage to spell check.
      * @return An array of misspelled words or null if no words are misspelled.
@@ -95,37 +73,34 @@ public class SpellCheckServiceImpl implements SpellCheckService
             return null;
         }
 
-        ArrayList errorList = new ArrayList();
+        List<String> errorList = new ArrayList<String>();
 
         // Tokenize the passage using spaces and punctionation.
         StringTokenizer st = new StringTokenizer( passage, " ,.!?;:" );
 
-        // Lock the service list.
-        synchronized ( m_svcObjList )
+        // Put the current set of services in a local field
+        // the field m_svcObjList might be modified concurrently
+        final List<DictionaryService> localServices = m_svcObjList;
+
+        // Loop through each word in the passage.
+        while ( st.hasMoreTokens() )
         {
-            // Loop through each word in the passage.
-            while ( st.hasMoreTokens() )
+            String word = st.nextToken();
+            boolean correct = false;
+
+            // Check each available dictionary for the current word.
+            for(final DictionaryService dictionary : localServices) {
+                if ( dictionary.checkWord( word ) )
+                {
+                    correct = true;
+                }
+            }
+
+            // If the word is not correct, then add it
+            // to the incorrect word list.
+            if ( !correct )
             {
-                String word = st.nextToken();
-                boolean correct = false;
-
-                // Check each available dictionary for the current word.
-                for ( int i = 0; ( !correct ) && ( i < m_svcObjList.size() ); i++ )
-                {
-                    DictionaryService dictionary = ( DictionaryService ) m_svcObjList.get( i );
-
-                    if ( dictionary.checkWord( word ) )
-                    {
-                        correct = true;
-                    }
-                }
-
-                // If the word is not correct, then add it
-                // to the incorrect word list.
-                if ( !correct )
-                {
-                    errorList.add( word );
-                }
+                errorList.add( word );
             }
         }
 
@@ -136,6 +111,6 @@ public class SpellCheckServiceImpl implements SpellCheckService
         }
 
         // Return the array of incorrect words.
-        return ( String[] ) errorList.toArray( new String[errorList.size()] );
+        return errorList.toArray( new String[errorList.size()] );
     }
 }
