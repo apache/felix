57c84fb7424ebbe4932ab9ea6175dfac9de84802
Carsten Ziegeler <cziegeler@apache.org>
2016-08-02 13:12:19 +0000

    FELIX-5317 : Use http whiteboard for filter registration
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/http/sslfilter@1754911 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/http/sslfilter/internal/SslFilterActivator.java
+++ src/main/java/org/apache/felix/http/sslfilter/internal/SslFilterActivator.java
@@ -18,12 +18,31 @@
  */
 package org.apache.felix.http.sslfilter.internal;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.servlet.Filter;
+
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
+import org.osgi.service.log.LogService;
 
 public class SslFilterActivator implements BundleActivator
 {
-    private HttpServiceTracker httpTracker;
+    /** Singleton filter to be registered with all http services. */
+    private final SslFilter filter = new SslFilter();
+
+    private volatile ServiceRegistration configReceiver;
+
+    private volatile ServiceRegistration filterReg;
+
     private LogServiceTracker logTracker;
 
     @Override
@@ -32,22 +51,67 @@ public class SslFilterActivator implements BundleActivator
         this.logTracker = new LogServiceTracker(context);
         this.logTracker.open();
 
-        this.httpTracker = new HttpServiceTracker(context);
-        this.httpTracker.open();
+        final Dictionary<String, Object> props = new Hashtable<String, Object>();
+        props.put(Constants.SERVICE_PID, SslFilter.PID);
+
+        this.configReceiver = context.registerService(ManagedService.class.getName(), new ServiceFactory()
+        {
+            @Override
+            public Object getService(Bundle bundle, ServiceRegistration registration)
+            {
+                return new ManagedService()
+                {
+                    @Override
+                    public void updated(@SuppressWarnings("rawtypes") Dictionary properties) throws ConfigurationException
+                    {
+                        configureFilters(properties);
+                    }
+                };
+            }
+
+            @Override
+            public void ungetService(Bundle bundle, ServiceRegistration registration, Object service)
+            {
+                // Nop
+            }
+        }, props);
+
+        final Dictionary<String, Object> properties = new Hashtable<String, Object>();
+        properties.put(Constants.SERVICE_VENDOR, "The Apache Software Foundation");
+        properties.put(Constants.SERVICE_DESCRIPTION, "Apache Felix HTTP SSL Filter");
+
+        // any context
+        properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_SELECT, "(" + HttpWhiteboardConstants.HTTP_WHITEBOARD_CONTEXT_NAME + "=*)");
+        properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_FILTER_PATTERN, "/");
+
+        this.filterReg = context.registerService(Filter.class.getName(), filter, properties);
+
+        SystemLogger.log(LogService.LOG_DEBUG, "SSL filter registered...");
     }
 
     @Override
     public void stop(final BundleContext context)
     {
-        if (this.httpTracker != null)
+        if ( this.filterReg != null )
         {
-            this.httpTracker.close();
-            this.httpTracker = null;
+            this.filterReg.unregister();
+            this.filterReg = null;
+            SystemLogger.log(LogService.LOG_DEBUG, "SSL filter unregistered...");
+        }
+        if (this.configReceiver != null)
+        {
+            this.configReceiver.unregister();
+            this.configReceiver = null;
         }
         if (this.logTracker != null)
         {
             this.logTracker.close();
             this.logTracker = null;
         }
+    }
+
+    void configureFilters(@SuppressWarnings("rawtypes") final Dictionary properties) throws ConfigurationException
+    {
+        this.filter.configure(properties);
     }
 }
