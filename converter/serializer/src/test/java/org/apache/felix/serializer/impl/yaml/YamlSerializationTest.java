1ee254fe48f86c1a90cdfd566f92de96298bcb8c
A. J. David Bosschaert <davidb@apache.org>
2016-09-27 08:23:40 +0000

    Felix Converter - additional YAML serializer unit test
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1762408 13f79535-47bb-0310-9956-ffa450edef68

--- converter/serializer/src/test/java/org/apache/felix/serializer/impl/yaml/YamlSerializationTest.java
+++ converter/serializer/src/test/java/org/apache/felix/serializer/impl/yaml/YamlSerializationTest.java
@@ -21,7 +21,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.felix.serializer.impl.yaml.YamlSerializerImpl;
+import org.apache.felix.serializer.impl.json.JsonSerializerImpl;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -92,5 +92,14 @@ public class YamlSerializationTest {
                 "  no: \n" +
                 "    maybe: false";
         assertEquals(expected, new YamlSerializerImpl().serialize(cm).toString().trim());
+    }
+
+    @Test
+    public void testEmptyMapSerialization() {
+        Map<?,?> m = new LinkedHashMap<>();
+        String expected = "{}";
+        assertEquals(expected, new JsonSerializerImpl().serialize(m).toString());
+        Map<?,?> m2 = new JsonSerializerImpl().deserialize(Map.class).from(expected);
+        assertEquals(m, m2);
     }
 }
