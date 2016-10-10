c05796f4b35347ddc4a423109f0a5e973bd494d2
A. J. David Bosschaert <davidb@apache.org>
2016-10-10 16:04:47 +0000

    Felix Converter - update ProviderType/ConsumerType annotations
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/converter/converter@1764143 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/osgi/converter/Converting.java
+++ src/main/java/org/osgi/converter/Converting.java
@@ -29,15 +29,17 @@ import org.osgi.annotation.versioning.ProviderType;
  */
 @ProviderType
 public interface Converting {
-    /**
-     * Convert the source object based on rules for the class being passed in.
-     * This method can be used to disambiguate objects that would match multiple
-     * conversion rules.
-     * @param type The type or class to be used as the source type for the conversion
-     * @return The current {@code Converting} object so that additional calls
-     *         can be chained.
-     */
-    Converting as(Class<?> type);
+	/**
+	 * Convert the source object based on rules for the class being passed in.
+	 * This method can be used to disambiguate objects that would match multiple
+	 * conversion rules.
+	 *
+	 * @param type The type or class to be used as the source type for the
+	 *            conversion
+	 * @return The current {@code Converting} object so that additional calls
+	 *         can be chained.
+	 */
+	Converting as(Class< ? > type);
 
 	/**
 	 * The default value to use when the object cannot be converted or in case
