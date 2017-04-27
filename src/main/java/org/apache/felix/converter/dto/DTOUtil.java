a1f4d112a31c6029cb9d6d69e9b437bfc956c05d
A. J. David Bosschaert <davidb@apache.org>
2017-04-27 14:02:03 +0000

    Felix Converter - move DTOUtil to separate package
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/converter/converter@1792898 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/converter/dto/DTOUtil.java
+++ src/main/java/org/apache/felix/converter/dto/DTOUtil.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.felix.converter;
+package org.apache.felix.converter.dto;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
