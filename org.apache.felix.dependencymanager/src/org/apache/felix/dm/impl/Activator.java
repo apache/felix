d45615f12260332189847a9395eac878ba999c5a
Pierre De Rop <pderop@apache.org>
2017-05-29 07:03:24 +0000

    FELIX-5619: MultiProperyFilterIndex memory consumption FELIX-4028: Extensible filter index mechanism
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/dependencymanager@1796576 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.felix.dependencymanager/src/org/apache/felix/dm/impl/Activator.java
+++ org.apache.felix.dependencymanager/src/org/apache/felix/dm/impl/Activator.java
@@ -19,6 +19,9 @@
 package org.apache.felix.dm.impl;
 
 import org.apache.felix.dm.ComponentExecutorFactory;
+import org.apache.felix.dm.FilterIndex;
+import org.apache.felix.dm.impl.index.ServiceRegistryCache;
+import org.apache.felix.dm.impl.index.ServiceRegistryCacheManager;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Filter;
@@ -27,40 +30,75 @@ import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
- * DependencyManager Activator used to track a ComponentExecutorFactory service optionally registered by 
- * a management agent bundle.
+ * DependencyManager Activator used to track a ComponentExecutorFactory service
+ * optionally registered by a management agent bundle.
  * 
  * @see {@link ComponentExecutorFactory}
  * @author <a href="mailto:dev@felix.apache.org">Felix Project Team</a>
  */
-public class Activator implements BundleActivator, ServiceTrackerCustomizer<ComponentExecutorFactory, ComponentExecutorFactory> {
-    private BundleContext m_context;
-    
+public class Activator implements BundleActivator {
+	private BundleContext m_context;
+	private ServiceTracker<ComponentExecutorFactory, ComponentExecutorFactory> m_execTracker;
+	private ServiceTracker<FilterIndex, FilterIndex> m_indexTracker;
+
 	@Override
-	public void start(BundleContext context) throws Exception {
+	public void start(BundleContext context) throws Exception {		
+		// make sure the backdoor is set in system properties (needed by tests)
+		ServiceRegistryCacheManager.init(); 
+
 		m_context = context;
-        Filter filter = context.createFilter("(objectClass=" + ComponentExecutorFactory.class.getName() + ")");
-        ServiceTracker<ComponentExecutorFactory, ComponentExecutorFactory> tracker = new ServiceTracker<>(context, filter, this);
-        tracker.open();
+		Filter execFilter = context.createFilter("(objectClass=" + ComponentExecutorFactory.class.getName() + ")");
+		m_execTracker = new ServiceTracker<>(context, execFilter, new ExecutorFactoryCustomizer());
+		m_execTracker.open();
+		
+		Filter indexFilter = context.createFilter("(objectClass=" + FilterIndex.class.getName() + ")");
+		m_indexTracker = new ServiceTracker<>(context, indexFilter, new FilterIndexCustomizer());
+		m_indexTracker.open();
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		if (m_execTracker != null) {
+			m_execTracker.close();
+		}
+		if (m_indexTracker != null) {
+			m_indexTracker.close();
+		}
 	}
 
-	@Override
-	public ComponentExecutorFactory addingService(ServiceReference<ComponentExecutorFactory> reference) {
-		ComponentExecutorFactory factory = (ComponentExecutorFactory) m_context.getService(reference);
-		ComponentScheduler.instance().bind(factory);
-		return factory;
-	}
+	private class ExecutorFactoryCustomizer implements ServiceTrackerCustomizer<ComponentExecutorFactory, ComponentExecutorFactory> {
+		@Override
+		public ComponentExecutorFactory addingService(ServiceReference<ComponentExecutorFactory> reference) {
+			ComponentExecutorFactory factory = (ComponentExecutorFactory) m_context.getService(reference);
+			ComponentScheduler.instance().bind(factory);
+			return factory;
+		}
 
-	@Override
-	public void modifiedService(ServiceReference<ComponentExecutorFactory> reference, ComponentExecutorFactory service) {
-	}
+		@Override
+		public void modifiedService(ServiceReference<ComponentExecutorFactory> reference, ComponentExecutorFactory service) {
+		}
 
-	@Override
-	public void removedService(ServiceReference<ComponentExecutorFactory> reference, ComponentExecutorFactory factory) {
-		ComponentScheduler.instance().unbind(factory);
+		@Override
+		public void removedService(ServiceReference<ComponentExecutorFactory> reference, ComponentExecutorFactory factory) {
+			ComponentScheduler.instance().unbind(factory);
+		}
+	}
+	
+	private class FilterIndexCustomizer implements ServiceTrackerCustomizer<FilterIndex, FilterIndex> {
+		@Override
+		public FilterIndex addingService(ServiceReference<FilterIndex> reference) {
+			FilterIndex index = m_context.getService(reference);
+			ServiceRegistryCacheManager.registerFilterIndex(index, m_context);
+			return index;
+		}
+
+		@Override
+		public void modifiedService(ServiceReference<FilterIndex> reference, FilterIndex service) {				
+		}
+
+		@Override
+		public void removedService(ServiceReference<FilterIndex> reference, FilterIndex index) {
+			ServiceRegistryCacheManager.unregisterFilterIndex(index);
+		}				
 	}
 }
