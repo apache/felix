e715d58d7800f854ad6595ab932ea0f746419ba5
Carsten Ziegeler <cziegeler@apache.org>
2017-11-08 15:19:00 +0000

    FELIX-5729 : NoSuchFieldError when @Reference referenceInterface type different than field type
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk@1814588 13f79535-47bb-0310-9956-ffa450edef68

--- tools/org.apache.felix.scr.generator/src/main/java/org/apache/felix/scrplugin/helper/ClassModifier.java
+++ tools/org.apache.felix.scr.generator/src/main/java/org/apache/felix/scrplugin/helper/ClassModifier.java
@@ -41,8 +41,9 @@ public abstract class ClassModifier {
      * Add bind/unbind methods
      * @param className       The class name in which the methods are injected
      * @param referenceName   Name of the reference
+     * @param referenceType   Type of the reference
      * @param fieldName       Name of the field
-     * @param typeName        Name of the type
+     * @param fieldType       Type of the field
      * @param createBind      Name of the bind method or null
      * @param createUnbind    Name of the unbind method or null
      * @param outputDirectory Output directory where the class file is stored
@@ -50,8 +51,9 @@ public abstract class ClassModifier {
      */
     public static void addMethods(final String className,
                            final String referenceName,
+                           final String referenceType,
                            final String fieldName,
-                           final String typeName,
+                           final String fieldType,
                            final boolean createBind,
                            final boolean createUnbind,
                            final ClassLoader classLoader,
@@ -105,12 +107,12 @@ public abstract class ClassModifier {
             if ( createBind ) {
                 logger.debug("Adding bind " + className + " " + fieldName);
 
-                createMethod(writer, className, referenceName, fieldName, typeName, true);
+                createMethod(writer, className, referenceName, referenceType, fieldName, fieldType, true);
             }
             if ( createUnbind ) {
                 logger.debug("Adding unbind " + className + " " + fieldName);
 
-                createMethod(writer, className, referenceName, fieldName, typeName, false);
+                createMethod(writer, className, referenceName, referenceType, fieldName, fieldType, false);
             }
 
             final FileOutputStream fos = new FileOutputStream(fileName);
@@ -121,26 +123,27 @@ public abstract class ClassModifier {
                 fos.close();
             }
         } catch (final Exception e) {
-            throw new SCRDescriptorException("Unable to add methods to " + className, typeName, e);
+            throw new SCRDescriptorException("Unable to add methods to " + className, referenceType, e);
         }
     }
 
-    private static void createMethod(final ClassWriter cw, final String className, final String referenceName, final String fieldName, final String typeName, final boolean bind) {
-        final org.objectweb.asm.Type type = org.objectweb.asm.Type.getType("L" + typeName.replace('.', '/') + ";");
+    private static void createMethod(final ClassWriter cw, final String className, final String referenceName, final String referenceTypeName, final String fieldName, final String fieldTypeName, final boolean bind) {
+        final org.objectweb.asm.Type referenceType = org.objectweb.asm.Type.getType("L" + referenceTypeName.replace('.', '/') + ";");
+        final org.objectweb.asm.Type fieldType = org.objectweb.asm.Type.getType("L" + fieldTypeName.replace('.', '/') + ";");
         final String methodName = (bind ? "" : "un") + "bind" + referenceName.substring(0, 1).toUpperCase() + referenceName.substring(1);
-        final MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PROTECTED, methodName, "(" + type.toString() + ")V", null, null);
+        final MethodVisitor mv = cw.visitMethod(Opcodes.ACC_PROTECTED, methodName, "(" + referenceType.toString() + ")V", null, null);
         mv.visitVarInsn(Opcodes.ALOAD, 0);
         if ( bind ) {
-            mv.visitVarInsn(type.getOpcode(Opcodes.ILOAD), 1);
-            mv.visitFieldInsn(Opcodes.PUTFIELD, className.replace('.', '/'), fieldName, type.toString());
+            mv.visitVarInsn(referenceType.getOpcode(Opcodes.ILOAD), 1);
+            mv.visitFieldInsn(Opcodes.PUTFIELD, className.replace('.', '/'), fieldName, fieldType.toString());
         } else {
-            mv.visitFieldInsn(Opcodes.GETFIELD, className.replace('.', '/'), fieldName, type.toString());
+            mv.visitFieldInsn(Opcodes.GETFIELD, className.replace('.', '/'), fieldName, fieldType.toString());
             mv.visitVarInsn(Opcodes.ALOAD, 1);
             final Label jmpLabel = new Label();
             mv.visitJumpInsn(Opcodes.IF_ACMPNE, jmpLabel);
             mv.visitVarInsn(Opcodes.ALOAD, 0);
             mv.visitInsn(Opcodes.ACONST_NULL);
-            mv.visitFieldInsn(Opcodes.PUTFIELD, className.replace('.', '/'), fieldName, type.toString());
+            mv.visitFieldInsn(Opcodes.PUTFIELD, className.replace('.', '/'), fieldName, fieldType.toString());
             mv.visitLabel(jmpLabel);
         }
         mv.visitInsn(Opcodes.RETURN);
