6777feaee7a09d31b5551d0f5042de73b68c006b
Georg Henzler <ghenzler@apache.org>
2019-01-09 08:51:54 +0000

    FELIX-5952 improved naming for sticky results
    git-svn-id: https://svn.apache.org/repos/asf/felix/trunk/healthcheck/api@1850827 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/felix/hc/api/HealthCheck.java
+++ src/main/java/org/apache/felix/hc/api/HealthCheck.java
@@ -35,14 +35,14 @@ public interface HealthCheck {
      * service with the same value for this property. The value of this property must be of type String. */
     String NAME = "hc.name";
 
+    /** Optional service property: tags for categorizing the health check services. The value of this property must be of type String or
+     * String array. */
+    String TAGS = "hc.tags";
+    
     /** Optional service property: the name of the MBean for registering the health check as an MBean. If this property is missing the
      * health check is not registered as a JMX MBean. If there is more than one service with the same value for this property, the one with
      * the highest service ranking is registered only. The value of this property must be of type String. */
     String MBEAN_NAME = "hc.mbean.name";
-
-    /** Optional service property: tags for categorizing the health check services. The value of this property must be of type String or
-     * String array. */
-    String TAGS = "hc.tags";
 
     /** Optional service property: If this property is set the health check will be executed asynchronously using the cron expression
      * provided. */
@@ -55,11 +55,12 @@ public interface HealthCheck {
      * specified in ms. */
     String RESULT_CACHE_TTL_IN_MS = "hc.resultCacheTtlInMs";
 
-    /** Optional service property: If given, warning results (that is WARN, CRITICAL or HEALTH_CHECK_ERROR) from the past executions will be
-     * taken into account as well for the given minutes (use Integer.MAX_VALUE for indefinitely). Useful for unhealthy system states that
-     * disappear but might leave the system at an inconsistent state (e.g. an event queue overflow). */
-    String WARNINGS_STICK_FOR_MINUTES = "hc.warningsStickForMinutes";
-
+    /** Optional service property: If given, non-ok results from past executions will be taken into account as well for the given seconds 
+     * (use Long.MAX_VALUE for indefinitely). Useful for unhealthy system states that disappear but might leave the system at an 
+     * inconsistent state (e.g. an event queue overflow where somebody needs to intervene manually) or for checks that should only go back 
+     * to OK with a delay (can be useful for load balancers). */
+    String KEEP_NON_OK_RESULTS_STICKY_FOR_SEC = "hc.keepNonOkResultsStickyForSec";
+    
     /** Execute this health check and return a {@link Result}.*/
     Result execute();
 }
